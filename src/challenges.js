2dbc1b1287b1daba3f9acf25e8b020beb3914cf6
Diego-AS-Mendes <dasm021@msn.com>
2021-08-31 15:40:17 -0300

    Requisito 2

--- src/challenges.js
+++ src/challenges.js
@@ -8,9 +8,13 @@ function compareTrue(value1, value2) {
 console.log(compareTrue(true, false));
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let areaTriangulo = (base * height) / 2
+  return areaTriangulo
 }
+console.log(calcArea(10, 50));
+console.log(calcArea(5, 2));
+console.log(calcArea(51, 1));
 
 // Desafio 3
 function splitSentence() {
