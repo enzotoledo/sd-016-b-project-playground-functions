fe1f0e607c39c02f33e93b73dcb5d5f7f4b0f6d4
Marcos Carvalho <marcosfilhosp@hotmail.com>
2021-08-31 16:55:27 -0300

    Challenge #9 Solved

--- src/challenges.js
+++ src/challenges.js
@@ -83,11 +83,22 @@ function fizzBuzz(numericArray) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(message) {
+  message = message.replace(/a/g, 1);
+  message = message.replace(/e/g, 2);
+  message = message.replace(/i/g, 3);
+  message = message.replace(/o/g, 4);
+  message = message.replace(/u/g, 5);
+  return message;
 }
-function decode() {
-  // seu código aqui
+
+function decode(message) {
+  message = message.replace(/1/g, 'a');
+  message = message.replace(/2/g, 'e');
+  message = message.replace(/3/g, 'i');
+  message = message.replace(/4/g, 'o');
+  message = message.replace(/5/g, 'u');
+  return message;
 }
 
 module.exports = {
