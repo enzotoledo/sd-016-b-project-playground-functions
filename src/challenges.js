379366197f6b6aa34dece4e508691f77bf6af347
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-08-31 18:14:02 -0300

    nono requisito concluido

--- src/challenges.js
+++ src/challenges.js
@@ -71,11 +71,42 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(crypt) {
+  let vogal = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  let encrypt = crypt.split('');
+
+  for (let key in encrypt) {
+    if (vogal[encrypt[key]]) {
+      encrypt[key] = vogal[encrypt[key]];
+    }
+  }
+
+  return encrypt.join('');
 }
-function decode() {
-  // seu código aqui
+
+function decode(crypt) {
+  let vogal = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  let encrypt = crypt.split('');
+
+  for (let index in encrypt) {
+    if (vogal[encrypt[index]]) {
+      encrypt[index] = vogal[encrypt[index]];
+    }
+  }
+
+  return encrypt.join('');
 }
 
 module.exports = {
