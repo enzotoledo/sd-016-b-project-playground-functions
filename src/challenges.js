421d0d9439de9f6943b148eb4ace96ac20dad534
Gabriel Pinheiro <g.felippe5965@gmail.com>
2021-08-31 15:18:15 -0300

    Exercicio 3 - Concluído.

--- src/challenges.js
+++ src/challenges.js
@@ -16,8 +16,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  let splitStr = str.split(' ');
+  return splitStr;
 }
 
 // Desafio 4
