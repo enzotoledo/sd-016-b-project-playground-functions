a01983efe0bec8f3b8832ac8e954fe3aa8c33e19
Lucas Oliveira <lucasoliveira226@gmail.com>
2021-08-31 16:35:30 -0300

    Desafios resolvidos até o 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,27 +1,42 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(value1, value2) {
   // seu código aqui
-  
+  if ((value1 === true) && (value2 === true)) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, altura) {
   // seu código aqui
+  return (base * altura) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   // seu código aqui
+  let stringSeparada = string.split(' ');
+  return stringSeparada;
 }
 
 // Desafio 4
-function concatName() {
+function concatName(array) {
   // seu código aqui
+  let ultimo = array[array.length - 1];
+  let primeiro = array[0];
+  let concat = `${ultimo}, ${primeiro}`;
+
+  return concat;
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  let winPoints = wins * 3;
+  let points = winPoints + ties;
+
+  return points;
 }
 
 // Desafio 6
