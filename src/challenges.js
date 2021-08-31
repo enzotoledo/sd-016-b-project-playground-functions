a286ba8e6970e6a251e38ac07618ae85e19986d5
Juliana Miranda <juliana.ms@live.com>
2021-08-31 18:52:20 -0300

    Desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -44,8 +44,19 @@ function highestCount(arrayNum) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = Math.abs(mouse - cat1);
+  let distanciaCat2 = Math.abs(mouse - cat2);
+
+  if ((distanciaCat1) < distanciaCat2) {
+    return 'cat1';
+  }
+  if (distanciaCat2 < distanciaCat1) {
+    return 'cat2';
+  }
+  if (distanciaCat1 === distanciaCat2) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
