e0064406cb837e45d56e73afebb5da447575ecc3
igor rafael <igor.rafael.briano@gmail.com>
2021-08-31 15:11:23 -0300

    finalizado exercicio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(ValorA,ValorB) {
+  if(ValorA && ValorB) {
+    return true;
+  }else{
+    return false;
+  }
 }
 
 // Desafio 2
