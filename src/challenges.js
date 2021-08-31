fdf4cd331db6eb1cdfb34283686019c39ef5d3bf
Rafael Catarino <rafaelscatarino@hotmail.com>
2021-08-31 15:31:56 -0300

    requisito 3

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,8 @@ function calcArea(base, heigth) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split(' ');
 }
 
 // Desafio 4
