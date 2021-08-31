23eddf3b65b740b0ebba4b54fdc752ebc22e6d04
Matheus Nugas <matheusnugas@gmail.com>
2021-08-31 15:34:08 -0300

    Exercicio 6 feito

--- src/challenges.js
+++ src/challenges.js
@@ -32,13 +32,35 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arr) {
+  let count = 0;
+  let largest = -Infinity;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  for (let j = 0; j < arr.length; j += 1) {
+    if (largest === arr[j]) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
+// https://stackoverflow.com/questions/52999505/how-do-i-find-the-largest-negative-integer-in-an-array Referencia a -Infinity encontrado neste link;
+
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(cat1, cat2, mouse) {
+  let dist1toMouse = Math.abs(cat1 - mouse);
+  let dist2toMouse = Math.abs(cat2 - mouse);
+  if (dist1toMouse > dist2toMouse) {
+    return "cat2";
+  } else if (dist1toMouse < dist2toMouse) {
+    return "cat1";
+  } else if (dist2toMouse == dist1toMouse) {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
