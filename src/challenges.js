be06e3aeaa1dd7395020b39e5cb9ea6a927a1a55
Bruno Marques <blmarques.dev@gmail.com>
2021-08-31 20:10:49 -0300

    seven requirement = done (Tom n Gerry, rsrs)

--- src/challenges.js
+++ src/challenges.js
@@ -45,8 +45,16 @@ function highestCount(listNumer) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    return 'cat1';
+  }
+  else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    return 'cat2';
+  } 
+  else {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
