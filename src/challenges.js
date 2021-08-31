556a7cf441c78951e92b7875d061500866669a8c
Renata Domingues <blackbirdfly.r@gmail.com>
2021-08-31 20:50:08 -0300

    Requisito 4

--- src/challenges.js
+++ src/challenges.js
@@ -24,10 +24,14 @@ console.log(splitSentence('vamo que vamo'));
 console.log(splitSentence('foguete'));
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let a = array[array.length - 1];
+  let b = array[0];
+  return `${a}, ${b}`;
 }
-
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+console.log(concatName(['captain', 'my', 'captain']));
 // Desafio 5
 function footballPoints() {
   // seu código aqui
