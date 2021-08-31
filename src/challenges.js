8e125e7b14978fdef43f22a1f37a5825c3b1541a
Leandro Boralli <leboralli@gmail.com>
2021-08-31 15:34:51 -0300

    exercicio 5

--- src/challenges.js
+++ src/challenges.js
@@ -28,8 +28,9 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontosVitorias = wins *3
+  return pontosVitorias + ties
 }
 
 // Desafio 6
