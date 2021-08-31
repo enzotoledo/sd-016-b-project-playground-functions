7b220dcfa24a5584a0ceb563132f4e1f7fb92578
Leandro Boralli <leboralli@gmail.com>
2021-08-31 15:12:38 -0300

    exercicio 2

--- src/challenges.js
+++ src/challenges.js
@@ -7,11 +7,10 @@ function compareTrue(param1, param2) {
   }
 
 }
-console.log(compareTrue(false, true))
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2
 }
 
 // Desafio 3
