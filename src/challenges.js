d77915d47e8dedf2908069c4270873e8c2d4bcca
Lucas <lb.camargo94@gmail.com>
2021-08-31 18:27:47 -0300

    Desafio 2 completed

--- src/challenges.js
+++ src/challenges.js
@@ -2,84 +2,78 @@
 // Playground Functions
 
 // Desafio 1
-console.log('Desafio 1')
 
-function compareTrue(num1, num2, num3) {
+
+function compareTrue(par1, par2) {
     // seu código aqui
-    if (num1 > num2 && num1 > num3) {
-        return true;
-    } else {
-        return false
-    }
+    return par1 && par2;
 }
 // testando a função
-console.log(compareTrue(1, 2, 3));
-console.log(compareTrue(3, 2, 1));
-console.log(compareTrue(4, 5, 6));
-console.log(compareTrue(6, 5, 4));
-console.log(compareTrue(7, 8, 9));
-console.log(compareTrue(9, 8, 7));
-console.log(compareTrue(-1, 0, 1));
-console.log(compareTrue(1, 0, -1));
+// console.log(compareTrue(true, false));
+// console.log(compareTrue(false, false));
+// console.log(compareTrue(false, true));
+// console.log(compareTrue(true, true));
 
 // Desafio 2
-console.log('Desafio 2')
-
-function calcArea() {
 
 
+function calcArea(base, height) {
+    // seu código aqui
+    return (base * height) / 2
 }
 // testando a função
-console.log((a));
+// console.log(calcArea(10, 50));
+// console.log(calcArea(5, 2));
+// console.log(calcArea(51, 1));
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
     // seu código aqui
-}
 
-// Desafio 4
-function concatName() {
-    // seu código aqui
-}
 
-// Desafio 5
-function footballPoints() {
-    // seu código aqui
-}
+    // Desafio 4
+    function concatName() {
+        // seu código aqui
+    }
 
-// Desafio 6
-function highestCount() {
-    // seu código aqui
-}
+    // Desafio 5
+    function footballPoints() {
+        // seu código aqui
+    }
 
-// Desafio 7
-function catAndMouse() {
-    // seu código aqui
-}
+    // Desafio 6
+    function highestCount() {
+        // seu código aqui
+    }
 
-// Desafio 8
-function fizzBuzz() {
-    // seu código aqui
-}
+    // Desafio 7
+    function catAndMouse() {
+        // seu código aqui
+    }
 
-// Desafio 9
-function encode() {
-    // seu código aqui
-}
+    // Desafio 8
+    function fizzBuzz() {
+        // seu código aqui
+    }
 
-function decode() {
-    // seu código aqui
-}
+    // Desafio 9
+    function encode() {
+        // seu código aqui
+    }
 
-module.exports = {
-    calcArea,
-    catAndMouse,
-    compareTrue,
-    concatName,
-    decode,
-    encode,
-    fizzBuzz,
-    footballPoints,
-    highestCount,
-    splitSentence,
-};
+    function decode() {
+        // seu código aqui
+    }
+
+    module.exports = {
+        calcArea,
+        catAndMouse,
+        compareTrue,
+        concatName,
+        decode,
+        encode,
+        fizzBuzz,
+        footballPoints,
+        highestCount,
+        splitSentence,
+    };
