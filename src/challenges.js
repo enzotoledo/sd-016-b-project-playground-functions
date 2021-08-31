af8dd6d47c9dc118c3b3db9f0e4d3fa666627cb9
Matheus Nugas <matheusnugas@gmail.com>
2021-08-31 16:09:03 -0300

    Exercicio 9 feito

--- src/challenges.js
+++ src/challenges.js
@@ -81,11 +81,13 @@ function fizzBuzz(arr) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(str) {
+  let encoded = str.replace(/[a]/g, "1").replace(/[e]/g, "2").replace(/[i]/g, "3").replace(/[o]/g, "4").replace(/[u]/g, "5");
+  return encoded;
 }
-function decode() {
-  // seu código aqui
+function decode(str) {
+  let decoded = str.replace(/[1]/g, "a").replace(/[2]/g, "e").replace(/[3]/g, "i").replace(/[4]/g, "o").replace(/[5]/g, "u");
+  return decoded;
 }
 
 module.exports = {
