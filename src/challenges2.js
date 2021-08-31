95790aebd8f64e48ca3465ef5ed879e7daa72586
Israel Sant'Anna <israel.santanna@gmail.com>
2021-08-31 16:38:23 -0300

    Satisfy BONUS requirement 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,8 +6,35 @@ function techList(techArray, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+function areValidNumbers(numbersArray) {
+  if (numbersArray.length !== 11) {
+    return {
+      isValid: false,
+      msg: 'Array com tamanho incorreto.',
+    };
+  }
+  if (numbersArray.some(function (number) {
+    return numbersArray.filter((value) => value === number).length >= 3 || number < 0 || number > 9;
+  })) {
+    return {
+      isValid: false,
+      msg: 'não é possível gerar um número de telefone com esses valores',
+    };
+  }
+  return { isValid: true, msg: '' };
+}
+function generatePhoneNumber(numbersArray) {
+  let isValidArray = areValidNumbers(numbersArray);
+  if (!isValidArray.isValid) return isValidArray.msg;
+  let formattedPhoneNumber = [];
+  for (let index = 0; index < numbersArray.length; index += 1) {
+    if (index === 0) formattedPhoneNumber.push('(');
+    formattedPhoneNumber.push(numbersArray[index]);
+    if (index === 1) formattedPhoneNumber.push(') ');
+    if (index === 6) formattedPhoneNumber.push('-');
+  }
+  return formattedPhoneNumber.join('');
 }
 
 // Desafio 12
