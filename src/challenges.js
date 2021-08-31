d7f258545652c05007d4fa334ee9f6fe1bf86f6a
Ary Barbosa <aryhertonbarbosa@gmail.com>
2021-08-31 16:30:34 -0300

    fifth question

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,10 @@ function concatName(str) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = (wins * 3) + ties;
+  return pontos;
+
 }
 
 // Desafio 6
