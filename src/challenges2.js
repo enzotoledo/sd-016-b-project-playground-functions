865490a4dea8e56722b5718af66843e7cb5fda99
Matheus Nugas <matheusnugas@gmail.com>
2021-08-31 19:02:27 -0300

    Exercicio 11 feito

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,32 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arr) {
+  let erroA = "Array com tamanho incorreto.";
+  let erroB = "não é possível gerar um número de telefone com esses valores";
+
+  if (arr.length !== 11) {
+    return erroA;
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    let count = 0;
+    if (arr[i] < 0) {
+      return erroB;
+    } else if (arr[i] > 9) {
+      return erroB;
+    }
+    for (let j = i; j < arr.length; j += 1) {
+      if (arr[i] === arr[j]) {
+        count += 1;
+      }
+      if (count >= 3) {
+        return erroB;
+      }
+    }
+  }
+  let phone = `(${arr[0]}${arr[1]}) ${arr[2]}${arr[3]}${arr[4]}${arr[5]}${arr[6]}-${arr[7]}${arr[8]}${arr[9]}${arr[10]}`;
+  return phone;
 }
 
 // Desafio 12
