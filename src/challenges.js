f906983835518b28637e5ed47883ab4bc3a97099
Everton-Curti <evercurti@gmail.com>
2021-08-31 15:31:27 -0300

    Requisito tres solucionado.

--- src/challenges.js
+++ src/challenges.js
@@ -13,8 +13,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(frase) {
+  let arraySplit = frase.split(' ');
+  return arraySplit;
 }
 
 // Desafio 4
