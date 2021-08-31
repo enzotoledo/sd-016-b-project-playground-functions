423cfd94b3b9e271da258c918d2e052a9d2506fc
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-08-31 18:38:16 -0300

    Bônus até o 11 tudo OK

--- src/challenges2.js
+++ src/challenges2.js
@@ -7,9 +7,24 @@ function techList(tecs, name) {
   return l;
 }
 
+// Quantidade de um valor numa array
+function quantasVezesAparece(v, a) {
+  let quantidade = 0;
+  for (let j of a) if (j === v) quantidade += 1;
+  return quantidade;
+}
+
+function testeNumeros(a) {
+  for (let i of a) if (i < 0 || i > 9 || (quantasVezesAparece(i, a) > 2)) return false;
+  return true;
+}
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(a) {
+  if (a.length !== 11) return 'Array com tamanho incorreto.';
+  if (testeNumeros(a)) {
+    return `(${a[0]}${a[1]}) ${a[2]}${a[3]}${a[4]}${a[5]}${a[6]}-${a[7]}${a[8]}${a[9]}${a[10]}`;
+  }
+  return 'não é possível gerar um número de telefone com esses valores';
 }
 
 // Desafio 12
