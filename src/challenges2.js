bc5c5470b59fe624fd7d6d34725c2e15c4c9bf16
Wellington <tinhoefaela@gmail.com>
2021-08-31 18:58:24 -0300

    Requisito de 10 a 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,9 @@
 // Desafio 10
 function techList(arr, name) {
   if(arr.length > 0){
-    let arrayStringSorted = arr.sort();
+    let arrSorted = arr.sort();
     let result = [];
-    for(i of arrayStringSorted){
+    for(i of arrSorted){
       result.push({tech: i, name: name});
     }
     return result;
@@ -13,13 +13,12 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber(arrayNumber) {
+function generatePhoneNumber(arr) {
   let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  let result = '(';
-  if(arrayNumber.length!=11){
+  if(arr.length!=11){
     return ('Array com tamanho incorreto.');
   }else{
-    for(let i of arrayNumber){
+    for(let i of arr){
       if((i<0)||(i>9)){
         return ('não é possível gerar um número de telefone com esses valores');
       }else{
@@ -30,16 +29,17 @@ function generatePhoneNumber(arrayNumber) {
       }
     }
   }
+  let result = '(';
   for(i=0;i<2;i+=1){
-    result += arrayNumber[i];
+    result += arr[i];
   }
   result += ') ';
   for(i=2;i<7;i+=1){
-    result += arrayNumber[i];
+    result += arr[i];
   }
   result += '-';
   for(i=7;i<11;i+=1){
-    result += arrayNumber[i];
+    result += arr[i];
   }
   return result;
 }
@@ -55,16 +55,7 @@ function triangleCheck(lineA, lineB, lineC) {
 
 // Desafio 13
 function hydrate(string) {
-  var numbers = string.match(/\d+/g).map(Number);
-  let sum = 0;
-  for(i of numbers){
-    sum += i;
-  }
-  if(sum === 1){
-    return (sum+' copo de água');
-  }else{
-    return (sum+' copos de água');
-  }
+  
 }
 
 module.exports = {
