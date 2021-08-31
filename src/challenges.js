ab86ce60c0ea0e3b535b61bb5486b3021dfe302f
OiEuSouOPi <rafasc866@gmail.com>
2021-08-31 15:08:36 -0300

    Desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,9 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(a,b) {
+if((a==true) && (b==true)){
+  return true;
 }
-
+  else {
+  return false;
+  }
+}
 // Desafio 2
 function calcArea() {
   // seu código aqui
