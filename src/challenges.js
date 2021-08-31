d8a5088fce1933b9a83a6e177609f9e699b79a85
Cleitonsub <cleitonrmacedo@gmail.com>
2021-08-31 16:01:44 -0300

    Terceiro desafio concluido

--- src/challenges.js
+++ src/challenges.js
@@ -10,8 +10,9 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(wordString) {
+  let result = wordString.split(' ');
+  return result;
 }
 
 // Desafio 4
