230750c1d59ca20e7af97860685de3c1efa213b7
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-08-31 15:27:02 -0300

    até o 2 OK

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,11 @@
 // Desafio 1
-function compareTrue(a,b) {
+function compareTrue(a, b) {
   return a && b;
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return base * height / 2;
 }
 
 // Desafio 3
