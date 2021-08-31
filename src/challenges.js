cbd4276b915df47e1099e67f67b4d158ece23e2a
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-08-31 14:40:20 -0300

    Requisito 1 - Completo

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valorA, valorB) {
+  if((valorA && valorB) == true){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 // Desafio 2
