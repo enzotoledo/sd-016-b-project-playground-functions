a52df57e5b0c5657a202184459ff9643e9bc0d49
Juliana Miranda <juliana.ms@live.com>
2021-08-31 16:59:32 -0300

    Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -24,8 +24,9 @@ function concatName(arrayDeString) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let totalPoints = (wins * 3) + (ties);
+  return totalPoints;
 }
 
 // Desafio 6
