1eb63cc05dd7448414008d905421cb77f2e3b41a
lucaseix1 <lucas.bastos.seixas@gmail.com>
2021-08-31 15:46:49 -0300

    Desafio-01

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  
+function compareTrue(number1, number2) {
+  if ((number1 == true) && (number2 == true)){
+    return true
+  }
+   else { return false
+  }
+
 }
 
 // Desafio 2
