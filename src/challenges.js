8726b3fc2de3fbaf219a42443979f5da78734125
Millene Galetti <mikagaletti1@gmail.com>
2021-08-31 17:55:24 -0300

    Requisito 8- finalizado

--- src/challenges.js
+++ src/challenges.js
@@ -1,63 +1,72 @@
 // Desafio 1
-function compareTrue(param1,param2) {
-  if((param1 === true) && (param2 === true)){
+function compareTrue(param1, param2) {
+  if ((param1 === true) && (param2 === true)) {
     return true;
-  }  else {
-    return false;
   }
-};
+  return false;
+}
 
 // Desafio 2
-function calcArea(base,height) {
-  return (base*height)/2;
-};
+function calcArea(base, height) {
+  return (base * height) / 2;
+}
 
 // Desafio 3
 function splitSentence(string) {
-  return string.split(" ");
-};
+  return string.split('');
+}
 
 // Desafio 4
 function concatName(arrayString) {
-  return (arrayString[arrayString.length - 1] + ", " + arrayString[0]);  
-};
+  return (arrayString[arrayString.length - 1] + ', ' + arrayString[0]);
+}
 
 // Desafio 5
-function footballPoints(wins , ties) {
-  let points = wins*3 + ties;
+function footballPoints(wins, ties) {
+  let points = wins * 3 + ties;
   return points;
-};
-
+}
 
 // Desafio 6
 function highestCount(numbersArray) {
-  let max = - Infinity;
+  let max = -Infinity;
   let repeat = 0;
-  for (let index of numbersArray){
-    if(index>max){
+  for (let index of numbersArray) {
+    if (index > max) {
       max = index;
       repeat = 1;
-    } else if (index === max){
+    } else if (index === max) {
       repeat += 1;
     }
   }
   return repeat;
-};
+}
 
 // Desafio 7
-function catAndMouse(mouse ,cat1 , cat2) {
-  if(Math.abs(mouse -cat1) < Math.abs(mouse -cat2)){
-    return "cat1"
-  } else if(Math.abs(mouse - cat1) > Math.abs(mouse - cat2)){
-    return "cat2"
-  } else {
-  return "os gatos trombam e o rato foge"
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(mouse - cat1) < Math.abs(mouse - cat2)) {
+    return 'cat1';
+  } if (Math.abs(mouse - cat1) > Math.abs(mouse - cat2)) {
+    return 'cat2';
   }
-};
+  return 'os gatos trombam e o rato foge';
+}
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numberArray) {
+  let fizbuz = [];
+  for (let index of numberArray){
+    if ((index % 3 === 0) && (index % 5 === 0)) {
+      fizbuz.push('fizzBuzz');
+    } else if (index % 3 === 0) {
+      fizbuz.push('fizz');
+    } else if (index % 5 === 0) {
+      fizbuz.push('buzz');
+    } else {
+      fizbuz.push('bug!');
+    }
+  }
+    return fizbuz;
 }
 
 // Desafio 9
