4e5c6ffd65c0ee511d3ac543c1420cd8ffe822a1
Helder Emanuel Alves de Oliveira <hlremanuel16@gmail.com>
2021-08-31 18:47:06 -0300

    resolve os quesitos de 1 a 5

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,38 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(p1, p2) {
   // seu código aqui
+  if (p1 === true && p2 === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(str) {
   // seu código aqui
+  return str.split(' ');
 }
 
 // Desafio 4
-function concatName() {
+function concatName(arr) {
   // seu código aqui
+  let resp = [];
+  resp[1] = ' ' + arr[0];
+  resp[0] = arr.reverse()[0];
+  return resp.join(',');
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return wins * 3 + ties
 }
 
 // Desafio 6
