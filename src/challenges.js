7adb945a0af288fce5e68ea367e041ce5bb88b44
Erik Duarte Rodrigues <erikduarterodrigues@gmail.com>
2021-08-31 17:29:50 -0300

    Desafios entregues

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,55 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(parametA, parametB) {
+  if (parametA === true && parametB === true) {
+    return true
+    } else {
+    return false
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base*height)/2
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(str) {
+  let strToArr = str.split(' ')
+  return strToArr
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrToStr) {
+  let firstName = arrToStr[0];
+  let lastName = arrToStr[arrToStr.length - 1];
+  let str = [lastName, firstName];
+  return str.join(', ')
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let jogosGanhos = wins * 3;
+  let jogosEmpatados = ties;
+  let pontos = jogosGanhos + jogosEmpatados;
+  return pontos
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayDeNumeros) {
+  let maiorNumero  = arrayDeNumeros[0];
+  let contadorNumero = 0;
+
+  for (let index = 0; index < arrayDeNumeros.length; index +=1) {
+    if (arrayDeNumeros[index] > maiorNumero) {
+      maiorNumero = arrayDeNumeros[index];
+    }
+  }
+  for (let index = 0; index < arrayDeNumeros.length; index +=1) {
+    if (arrayDeNumeros[index] === maiorNumero) {
+      contadorNumero += 1;
+    }
+  }
+  return contadorNumero;
 }
 
 // Desafio 7
