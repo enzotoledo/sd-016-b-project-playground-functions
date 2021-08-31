b5c03f96948d5942541fe0e2c60646532c5ca339
Hugo Maximiano <hugomaximiano@hotmail.com>
2021-08-31 18:58:02 -0300

    desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -13,28 +13,41 @@ function calcArea(base, height) {
 }
 
 // Desafio 3
-function splitSentence() {
-  
+function splitSentence(sentence, separator) {
+ return sentence.split(" ");
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(names) {
+  return [names.length] + (names[0]);
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+ return ((wins*3) + ties);
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(numbers) {
+let counter = [];
+let higthNumber = Math.max(numbers);
+
+  for (let index = 0; index < numbers.length; index+=1) {
+   if (higthNumber === numbers[index]) {
+     counter +=1;
+   }
+  return counter;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(cat1, cat2, mouse) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
+    return 'cat1';
+  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+    return 'cat2';
+  } else {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
