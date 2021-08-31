2204d4ea0d004b333e0a9558673a5d7ddde87b98
MychaelJales <mychael_duarte@hotmail.com>
2021-08-31 18:33:07 -0300

    finaliza o desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -96,11 +96,22 @@ function fizzBuzz(arrayNumbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(stringCode) {
+  let stringEncode = stringCode.replace(/a/g, '1');
+  stringEncode = stringEncode.replace(/e/g, '2');
+  stringEncode = stringEncode.replace(/i/g, '3');
+  stringEncode = stringEncode.replace(/o/g, '4');
+  stringEncode = stringEncode.replace(/u/g, '5');
+  return stringEncode;
 }
-function decode() {
-  // seu código aqui
+
+function decode(stringEncode) {
+  let stringDecode = stringEncode.replace(/1/g, 'a');
+  stringDecode = stringDecode.replace(/2/g, 'e');
+  stringDecode = stringDecode.replace(/3/g, 'i');
+  stringDecode = stringDecode.replace(/4/g, 'o');
+  stringDecode = stringDecode.replace(/5/g, 'u');
+  return stringDecode;
 }
 
 module.exports = {
