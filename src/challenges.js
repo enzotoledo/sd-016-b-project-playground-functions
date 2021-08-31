2d388b0d40ca57dea9cc670e9d56716f53981f25
Gabriel A <gabrielaug4@gmail.com>
2021-08-31 19:20:59 -0300

    requirement 09

--- src/challenges.js
+++ src/challenges.js
@@ -93,14 +93,58 @@ function fizzBuzz(array) {
   }
   return emptyArray;
 }
-console.log(fizzBuzz([9, 25]));
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let emptyArray = [];
+
+  for(let index in string){
+    if(string[index] === 'a'){
+      emptyArray.push('1');
+    }
+    else if(string[index] === 'e'){
+      emptyArray.push('2');
+    }
+    else if(string[index]=== 'i'){
+      emptyArray.push('3');
+    }
+    else if(string[index] === 'o'){
+      emptyArray.push('4');
+    }
+    else if(string[index] === 'u'){
+      emptyArray.push('5');
+    }
+    else{
+      emptyArray.push(string[index]);
+    }
+  }
+  return emptyArray.join("");
 }
-function decode() {
-  // seu código aqui
+
+function decode(string){
+  let emptyArray = [];
+
+  for(let index in string){
+    if(string[index] === '1'){
+      emptyArray.push('a');
+    }
+    else if(string[index] === '2'){
+      emptyArray.push('e');
+    }
+    else if(string[index]=== '3'){
+      emptyArray.push('i');
+    }
+    else if(string[index] === '4'){
+      emptyArray.push('o');
+    }
+    else if(string[index] === '5'){
+      emptyArray.push('u');
+    }
+    else{
+      emptyArray.push(string[index]);
+    }
+  }
+  return emptyArray.join("");
 }
 
 module.exports = {
