285a937dc51a273bd87cfa051f1f1136b5fc5fe3
igor rafael <igor.rafael.briano@gmail.com>
2021-08-31 16:28:37 -0300

    finalizado exercicio 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,43 +1,63 @@
 // Desafio 1
-function compareTrue(ValorA,ValorB) {
+function compareTrue (ValorA, ValorB) {
   if(ValorA && ValorB) {
-    return true
-  }else{
-    return false
+    return true;
+  }else {
+    return false;
   }
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  return ((base*height)/2)
+function calcArea(base, height) {
+  return ((base * height) / 2);
 }
 
 // Desafio 3
 function splitSentence(texto) {
-  return arraytexto = texto.split(" ")
+  let arraytexto = texto.split(' ');
+  return arraytexto;
 }
 
 // Desafio 4
 function concatName(recebearray) {
-  let lastname = recebearray[recebearray.length-1];
+  let lastname = recebearray[recebearray.length - 1];
   let firstname = recebearray[0];
-  
-  return lastname + ", " + firstname
+  let text = lastname + ", "  + firstname;
+  return text;
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+// Desafio 6//
+function highestCount(arraynumber) {
+  let organizedNumber = arraynumber.sort();
+  let quants = 0;
+
+  for (let i = organizedNumber.length - 1; i >= 0; i -= 1) {
+    if (organizedNumber[organizedNumber.length - 1] <= organizedNumber[i]) {
+      quants += 1;
+    }
+  }
+
+  return quants;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distca1 = Math.abs(mouse - cat1);
+  let distca2 = Math.abs(mouse - cat2);
+  let quempega = '';
+  if (distca1 > distca2) {
+    quempega = 'cat2';
+  } else if (distca1 < distca2) {
+    quempega = 'cat1';
+  } else {
+    quempega = 'os gatos trombam e o rato foge';
+  }
+  return quempega;
 }
 
 // Desafio 8
