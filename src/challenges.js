278668e066679463c4466cdd1c85ce6625d07b07
Bruno Melo <melobrunoti@gmail.com>
2021-08-31 14:57:40 -0300

    Requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,10 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(value1, value2) {
   // seu código aqui
-  
+  if (value1 === true && value2 === true) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
