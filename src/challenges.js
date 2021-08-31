f2c06eba6ed909b2d651c590083db06056714c20
Gabriel Pinheiro <g.felippe5965@gmail.com>
2021-08-31 17:51:25 -0300

    Exercicio 8 - Concluído.

--- src/challenges.js
+++ src/challenges.js
@@ -65,8 +65,21 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+// eslint-disable-next-line complexity
+function fizzBuzz(array) {
+  let newArray = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 5 === 0 && array[index] % 3 === 0) {
+      newArray.push('fizzBuzz');
+    } else if (array[index] % 5 === 0) {
+      newArray.push('buzz');
+    } else if (array[index] % 3 === 0) {
+      newArray.push('fizz');
+    } else {
+      newArray.push('bug!');
+    }
+  }
+  return newArray;
 }
 
 // Desafio 9
