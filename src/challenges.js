81b7ecc00e6cee4a3c190647fba07d51a2a2531f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-08-31 17:39:08 -0300

    até o 10 tudo OK

--- src/challenges.js
+++ src/challenges.js
@@ -75,18 +75,42 @@ function fizzOuBuzz(v) {
 // Desafio 8
 function fizzBuzz(arr) {
   let a = [];
-  for (let i of arr) {
-    a.push(fizzOuBuzz(i));
-  }
+  for (let i of arr) a.push(fizzOuBuzz(i));
   return a;
 }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
+// Codifica as vogais
+let ksig = [undefined, 'a', 'e', 'i', 'o', 'u'];
+
+// Substitui caractere por numeral
+function substituiVogal(a, b) {
+  let r = [];
+  for (let i of a) {
+    if (b.indexOf(i) < 1) r.push(i);
+    else r.push(b.indexOf(i));
+  }
+  return r;
 }
-function decode() {
-  // seu código aqui
+
+// Substitui numeral por caractere
+function substituiNumeral(a, b) {
+  let r = [];
+  for (let i of a) {
+    if (i > 0) r.push(b[i]);
+    else r.push(i);
+  }
+  return r;
+}
+
+// Desafio 9
+function encode(s) {
+  s = s.split('');
+  return substituiVogal(s, ksig).join('');
+}
+
+function decode(s) {
+  s = s.split('');
+  return substituiNumeral(s, ksig).join('');
 }
 
 module.exports = {
