8cca81fd3cc9332613e0ac5bdc5b0591a4f15c21
lucas <araujo2012@hotmail.com>
2021-08-31 17:11:56 -0300

    Ajustes challenges.js de acordo com lint - incompleto

--- src/challenges.js
+++ src/challenges.js
@@ -5,7 +5,7 @@ function compareTrue(a, b) {
 
 // Desafio 2
 function calcArea(base, height) {
-  return (base*height/2);
+  return ((base * height) / 2);
 }
 
 // Desafio 3
@@ -17,23 +17,25 @@ function splitSentence(stringToSplit) {
 
 // Desafio 4
 function concatName(arrayString) {
-  return(arrayString.pop()+', '+arrayString[0]);
+  let result = '';
+  result = arrayString[arrayString.length - 1] + ', ' + arrayString[0];
+  return result;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  return (wins*3+ties);
+  return (wins * 3 + ties);
 }
 
 // Desafio 6
 function highestCount(arrayNumbers) {
   let max = -Infinity;
   let count = 0;
-  for(i of arrayNumbers){
-    if(i>max){
+  for (let i of arrayNumbers) {
+    if (i > max) {
       max = i;
       count = 1;
-    }else if(i===max){
+    } else if (i === max) {
       count += 1;
     }
   }
@@ -42,28 +44,28 @@ function highestCount(arrayNumbers) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  let dist1 = Math.abs(mouse-cat1);
-  let dist2 = Math.abs(mouse-cat2);
-  if(dist1 === dist2){
-    return("os gatos trombam e o rato foge");
-  }else if(dist1<dist2){
-    return('cat1');
-  }else{
-    return('cat2');
+  let dist1 = Math.abs(mouse - cat1);
+  let dist2 = Math.abs(mouse - cat2);
+  if (dist1 === dist2) {
+    return ('os gatos trombam e o rato foge');
   }
+  if (dist1 < dist2) {
+    return ('cat1');
+  }
+  return ('cat2');
 }
 
 // Desafio 8
 function fizzBuzz(arrayNumbers) {
   let result = [];
-  for(i of arrayNumbers){
-    if((i%3==0)&&(i%5==0)){
+  for (let i of arrayNumbers) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
       result.push('fizzBuzz');
-    }else if(i%3==0){
+    } else if (i % 3 === 0) {
       result.push('fizz');
-    }else if(i%5==0){
+    } else if (i % 5 === 0) {
       result.push('buzz');
-    }else{
+    } else {
       result.push('bug!');
     }
   }
@@ -78,24 +80,23 @@ function encode(string) {
     i: 3,
     o: 4,
     u: 5,
-  }
+  };
   let result = '';
-  for(j of string){
+  for (let j of string) {
     let z = 0;
-    for(k in logic){
-      if(j==k){
+    for (let k in logic) {
+      if (j === k) {
         result += logic[k];
         z = 1;
         break;
       }
     }
-    if(z===0){
+    if (z === 0) {
       result += j;
     }
   }
   return result;
 }
-
 
 function decode(string) {
   let logic = {
@@ -104,18 +105,18 @@ function decode(string) {
     i: 3,
     o: 4,
     u: 5,
-  }
+  };
   let result = '';
-  for(j of string){
+  for (let j of string) {
     let z = 0;
-    for(k in logic){
-      if(j==logic[k]){
+    for (let k in logic) {
+      if (j === logic[k]) {
         result += k;
         z = 1;
         break;
       }
     }
-    if(z===0){
+    if (z === 0) {
       result += j;
     }
   }
