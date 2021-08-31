409e5ac18cd2d6a9b03ea5a884312741612bc996
Arthur Silva <arthur1silva1r@gmail.com>
2021-08-31 15:17:26 -0300

    requisitos 4 ao 9

--- src/challenges.js
+++ src/challenges.js
@@ -14,42 +14,135 @@ function calcArea(base,altura) {
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(string) {
   let stringSeparada = string.split(' ')
   return stringSeparada
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  return (`${array[array.length-1]}, ${array[0]}`)
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins,ties) {
+  let pontos = (wins * 3) + (ties)
+  return pontos
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(array) {
+  let maior = 0;
+  let repeticoes = 0;
+  let todosNegativos = false;
+  for(let i =0; i < array.length; i++){
+    if(array[i] < maior){
+        todosNegativos = true
+    }else{
+      break
+    }
+  }
+  if (todosNegativos == true){
+    return 1
+  }
+  for(let i =0; i < array.length; i++){
+      if(array[i] > maior){
+          maior = array[i]
+      }
+  }
+  if(maior < 0){
+    return 1
+  }
+  for (let y = 0; y < array.length; y++) {
+      if(array[y] == maior){
+          repeticoes++
+      }  
+  }
+  return repeticoes
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse,cat1,cat2) {
+  if(mouse + cat1 > mouse + cat2){
+      return 'cat1'
+  }else if(mouse + cat2 > mouse + cat1){
+      return 'cat2'
+  }else{
+      return 'os gatos trombam e o rato foge'
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let arrayModificado = array
+  for(let i = 0; i < arrayModificado.length; i++){
+    if ((arrayModificado[i] % 3 == 0) && (arrayModificado[i] % 5 != 0)) {
+      arrayModificado[i] = 'fizz'
+    }else if((arrayModificado[i] % 5 == 0) && (arrayModificado[i] % 3 != 0)){
+      arrayModificado[i] = 'buzz'
+    }else if((arrayModificado[i] % 5 == 0) && (arrayModificado[i] % 3 == 0)){
+      arrayModificado[i] = 'fizzBuzz'
+    }else{
+      arrayModificado[i] = 'bug!'
+    }
+  }
+  return arrayModificado
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let stringModificada = []
+  let codificada = ''
+
+  for(let i = 0; i < string.length; i++){
+    let seraAdd = string[i];
+    if(seraAdd == 'a'){
+      seraAdd = '1'
+    }else if(seraAdd == 'e'){
+      seraAdd = '2'
+    }else if(seraAdd == 'i'){
+      seraAdd = '3'
+    }else if(seraAdd == 'o'){
+      seraAdd = '4'
+    }else if(seraAdd == 'u'){
+      seraAdd = '5'
+    }
+    stringModificada.push(seraAdd)
+  }
+  for(let y = 0; y < stringModificada.length; y++){
+    codificada += stringModificada[y]
+  }
+  return codificada
+
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  let stringModificada = []
+  let decofidicada = ''
+
+  for(let i = 0; i < string.length; i++){
+    let seraAdd = string[i];
+
+    if(seraAdd == '1'){
+        seraAdd = 'a'
+    }else if(seraAdd == '2'){
+        seraAdd = 'e'
+    }else if(seraAdd == '3'){
+        seraAdd = 'i'
+    }else if(seraAdd == '4'){
+        seraAdd = 'o'
+    }else if(seraAdd == '5'){
+        seraAdd = 'u'
+    }
+
+    stringModificada.push(seraAdd)
+
+    }
+
+  for(let y = 0; y < stringModificada.length; y++){
+    decofidicada += stringModificada[y]
+  }
+
+  return decofidicada
 }
 
 module.exports = {
