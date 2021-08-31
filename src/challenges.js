b1faf3beca5bfa924372f01b53d9a7ae3e5a3ebf
Matheus Nugas <matheusnugas@gmail.com>
2021-08-31 15:00:05 -0300

    Exercicios 1 e 2 feitos

--- src/challenges.js
+++ src/challenges.js
@@ -1,12 +1,15 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-
+function compareTrue(valueA, valueB) {
+  if (valueA && valueB === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
