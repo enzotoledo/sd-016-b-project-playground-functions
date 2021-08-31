366f5ca2ad53f92113bf7e75327e017555206c61
Alissandra <alissandraraysa@gmail.com>
2021-08-31 17:35:03 -0300

    atualiza desafio 7

--- src/challenges.js
+++ src/challenges.js
@@ -62,15 +62,16 @@ function highestCount(numbres) {
 function catAndMouse(mouse,cat1,cat2) {
   // seu código aqui
   
-  if (Math.abs (cat1*mouse) < Math.abs(cat2*mouse)){
+  if (Math.abs (cat1 - mouse) < Math.abs(cat2 - mouse)){
     return "cat1"
-  } else if (Math.abs(cat2*mouse) < Math.abs(cat1*mouse)){
+  } else if (Math.abs(cat2 - mouse) < Math.abs(cat1- mouse)){
     return "cat2"
   } else {
     return "os gatos trombam e o rato foge"
   }
 
 }
+console.log(catAndMouse(6,3,5))
 
 // Desafio 8
 function fizzBuzz(numbresFB) {
