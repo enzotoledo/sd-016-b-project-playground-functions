8f7a0a76406a07c38041dfde6213de9281eac513
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-08-31 15:16:56 -0300

    segundo e terceiro requisitos concluidos

--- src/challenges.js
+++ src/challenges.js
@@ -4,13 +4,13 @@ function compareTrue(value1, value2) {
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(value) {
+  return value.split(' ');
 }
 
 // Desafio 4
