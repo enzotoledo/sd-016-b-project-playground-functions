ddeb959da1fd1d33ed7eab1e8c1f1c16505395fd
Ary Barbosa <aryhertonbarbosa@gmail.com>
2021-08-31 15:09:11 -0300

    First change

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,11 @@
 // Desafio 1
-function compareTrue() {
-  var
+function compareTrue(a,b) {
+  if ((a === true) && (b === true)) {
+    return true;
+  }else {
+    return false;
+  }
+
 }
 
 // Desafio 2
