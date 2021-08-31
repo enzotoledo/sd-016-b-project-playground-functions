ddfdf9733413d59a46d7b4fd4c892592dad4f3e8
Everton-Curti <evercurti@gmail.com>
2021-08-31 16:10:37 -0300

    Requisito cinco solucionado.

--- src/challenges.js
+++ src/challenges.js
@@ -27,8 +27,10 @@ function concatName(array) {
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  wins *= 3;
+  ties *= 1;
+  return wins + ties;
 }
 
 // Desafio 6
