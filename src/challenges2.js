e170ac4c862c992d1b7434edec11999f60b372c6
OiEuSouOPi <rafasc866@gmail.com>
2021-09-01 20:33:03 -0300

    vamos la

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,13 +1,31 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech,name) {
+  let lista = [];
+  for(let objetos=0; objetos<tech.length; objetos+=1){
+  {
+    lista[objetos],
+    lista.tech =tech[objetos],
+    lista.name= name
+  };
+  return lista;
 }
-
+console.log(techList(["array,css"],"rafael"));
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(array) {  
+  if(array<array[11] || array>array[11]){
+    array = "Array com tamanho incorreto."
+  }
+  for(let index=0; index<array.length)
+  if(array=array[11]){
+  array.splice(7,0,"-")
+  array.splice(2,0,")")
+  array.unshift("(")
 
+  array=array.join("")
+  }
+  return array
+}
+console.log(generatePhoneNumber(["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "1"]))
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
