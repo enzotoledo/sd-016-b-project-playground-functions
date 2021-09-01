ff35dee799dfb029b1ef2cd989f28fa49b8adef3
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-01 11:07:51 -0300

    Requisito 10 - Completo

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,18 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(arrayTecnologias, nome) { /* Para a resolucao desse exercicio utilizei a ajuda do aluno : victor shin kamiguchi */
+  let tecnologiasAAprender = [];
+  if (arrayTecnologias.length > 0) {
+    arrayTecnologias.sort();
+    for (let index of arrayTecnologias) {
+      tecnologiasAAprender.push({
+        tech : index,
+        name: nome
+      });
+    }
+    return tecnologiasAAprender;
+  } 
+  return 'Vazio!';
+  }
 
 // Desafio 11
 function generatePhoneNumber() {
