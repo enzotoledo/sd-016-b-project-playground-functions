b04ba573ae2d97f03bf33ab5989ba4375b62ca98
Celso Fiorani <celsofiorani@gmail.com>
2021-09-01 12:21:53 -0300

    Parte 6

--- src/challenges.js
+++ src/challenges.js
@@ -22,19 +22,12 @@ function concatName(string) {
  return string[string.length -1]+", "+ string [0]; 
 } 
 
-
 // Desafio 5
 function footballPoints(wins,ties) {
   return wins*3 + ties
 }
 
 // Desafio 6
-// Escreva uma função chamada highestCount que, ao receber uma array de números, 
-// retorne a quantidade de vezes que o maior deles se repete.
-// Exemplo: caso o parâmetro de highestCount seja uma array com valores 
-// [9, 1, 2, 3, 9, 5, 7], a função deverá retornar 2, que é a quantidade de vezes que o número 9 
-// (maior número do array) se repete.
-// [9, 1, 2, 3, 9, 5, 7]
 function highestCount(param1) {
   let maiorNumero =param1[0];
   let qntsRepete = 0;
@@ -51,7 +44,6 @@ for (let index = 0; index < param1.length; index+=1){
 }
 return qntsRepete;
 }
-console.log(highestCount([9, 1, 9, 3, 9, 5, 7]))
 
 // Desafio 7
 function catAndMouse() {
