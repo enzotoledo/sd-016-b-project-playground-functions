ffa34126ea729f1b10a798ace537dfcb6550cc60
Marcio Junior <marcio.mj70@gmail.com>
2021-08-31 22:14:57 -0300

    feat: catAndMouse

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,5 @@
 // Desafio 1
 function compareTrue(valueOne, valueTwo) {
-  // seu código aqui
   if ((valueOne === true) && (valueTwo === true)) {
     return true;
   }
@@ -10,7 +9,6 @@ function compareTrue(valueOne, valueTwo) {
 
 // Desafio 2
 function calcArea(base, height) {
-  // seu código aqui
   const area = (base * height) / 2;
 
   return area;
@@ -18,13 +16,11 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(phrase) {
-  // seu código aqui
   return phrase.split(' ');
 }
 
 // Desafio 4
 function concatName(phrase) {
-  // seu código aqui
   const firstName = phrase[0];
   const lastName = phrase[phrase.length - 1];
 
@@ -33,7 +29,6 @@ function concatName(phrase) {
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  // seu código aqui
   const winPoints = wins * 3;
   const tiesPoints = ties;
 
@@ -42,10 +37,8 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(numbers) {
-  // seu código aqui
   let count = 0;
   let highestNumber = numbers[0];
-  // Descobre o maior numero
   for (const number of numbers) {
     if (number > highestNumber) {
       highestNumber = number;
@@ -60,8 +53,20 @@ function highestCount(numbers) {
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1Distance = cat1 - mouse;
+  let cat2Distance = cat2 - mouse;
+
+  if (cat1Distance < 0) cat1Distance *= -1;
+  if (cat1Distance > cat2Distance) {
+    return 'cat2';
+  }
+
+  if (cat2Distance > cat1Distance) {
+    return 'cat1';
+  }
+
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
