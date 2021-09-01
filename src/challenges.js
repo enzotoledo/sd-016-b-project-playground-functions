d62a1366f1a23fa361ceab61f40a46c3dc2b77ad
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-09-01 18:13:15 -0300

    Middle way challenge 11 and partial correction of lint

--- src/challenges.js
+++ src/challenges.js
@@ -2,9 +2,7 @@
 function compareTrue(valueFirst, valueSecond) {
   if (valueFirst === true && valueSecond === true) {
     return true;
-  } else {
-    return false;
-  }
+  } return false;
 }
 
 // Desafio 2
@@ -15,13 +13,13 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(splitStringInSpaces) {
-  let newString = splitStringInSpaces.split(" ");
+  let newString = splitStringInSpaces.split(' ');
   return newString;
 }
 
 // Desafio 4
 function concatName(FirstAndLast) {
-  let stringTwo = `${FirstAndLast[FirstAndLast.length -1]}, ${FirstAndLast[0]}`;
+  let stringTwo = `${FirstAndLast[FirstAndLast.length - 1]}, ${FirstAndLast[0]}`;
   return stringTwo;
 }
 
@@ -36,12 +34,9 @@ function highestCount(arrayOfNumbers) {
   let largestNumber = null;
   let countingTimes = 0;
 
-  for (index = 0; index < arrayOfNumbers.length; index += 1) {
+  for (let index = 0; index < arrayOfNumbers.length; index += 1) {
     let iterationNumber = arrayOfNumbers[index];
-    if (largestNumber === null) {
-      largestNumber = iterationNumber;
-      countingTimes = 1;
-    } else if (largestNumber < iterationNumber) {
+    if (largestNumber === null || largestNumber < iterationNumber) {
       largestNumber = iterationNumber;
       countingTimes = 1;
     } else if (largestNumber === iterationNumber) {
@@ -58,17 +53,16 @@ function catAndMouse(mouse, cat1, cat2) {
   let distanceC2 = Math.abs(cat2 - mouse);
 
   if (distanceC1 < distanceC2) {
-    return "cat1";
-  } else if (distanceC2 < distanceC1) { 
-    return "cat2";
-  } else {
-    return "os gatos trombam e o rato foge";
+    return 'cat1';
   }
+  if (distanceC2 < distanceC1) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
 function fizzBuzz(bigArray) {
-
   let arrayFizzBuzz = [];
 
   for(let index = 0; index < bigArray.length; index += 1) {
@@ -87,28 +81,32 @@ function fizzBuzz(bigArray) {
 
 // Desafio 9
 function encode(findVowelsInString) {
-  let encodedString = "";
+  let encodedString = '';
 
-  for(let index = 0; index < findVowelsInString.length; index += 1) {
+  for (let index = 0; index < findVowelsInString.length; index += 1) {
     if (!'aeiou'.includes(findVowelsInString[index])) {
       encodedString += findVowelsInString[index];
     } else if ('aeiou'.includes(findVowelsInString[index])) {
-      encodedString += findVowelsInString[index].replace("a", 1).replace("e", 2).replace("i", 3).replace("o", 4).replace("u", 5);
-      }
+      encodedString += findVowelsInString[index]
+        .replace('a', 1).replace('e', 2).replace('i', 3).replace('o', 4)
+        .replace('u', 5);
+    }
   }
   return encodedString;
 }
 
 function decode(findNumbersInString) {
 
-  let decodedString = "";
+  let decodedString = '';
 
-  for(let index = 0; index < findNumbersInString.length; index += 1) {
+  for (let index = 0; index < findNumbersInString.length; index += 1) {
     if (!'12345'.includes(findNumbersInString[index])) {
       decodedString += findNumbersInString[index];
     } else if ('12345'.includes(findNumbersInString[index])) {
-      decodedString += findNumbersInString[index].replace(1, "a").replace(2, "e").replace(3, "i").replace(4, "o").replace(5, "u");
-      }
+      decodedString += findNumbersInString[index]
+        .replace(1, 'a').replace(2, 'e').replace(3, 'i').replace(4, 'o')
+        .replace(5, 'u');
+    }
   }
   return decodedString;
 }
