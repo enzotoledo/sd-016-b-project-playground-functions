b13a2369787dd8f9585d505fddff29ed5bf85b53
Douglas Toledo <Douglas Toledo - Duskimm>
2021-09-01 10:49:48 -0300

    exercicio 8

--- src/challenges.js
+++ src/challenges.js
@@ -47,9 +47,19 @@ function catAndMouse(mouse, cat1, cat2) {
   }
 }
 
-// Desafio 8
 function fizzBuzz() {
-  // seu código aqui
+  for(i = 0; i < array.length; i += 1){
+    if(array[i] % 15 == 0 ){
+      array[i] = "fizzBuzz";
+    }else if(array[i] % 3 == 0){
+      array[i] = "fizz";
+    }else if(array [i] % 5 == 0){
+      array[i] = "buzz";
+    }else{
+      array[i] = "bug!";
+    }
+  }
+  return array;
 }
 
 // Desafio 9
