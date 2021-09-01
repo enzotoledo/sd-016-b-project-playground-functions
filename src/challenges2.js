84eb9d787b685509d027a0643ee9c2c3a142b1c3
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-01 10:44:14 -0300

    C11 Requisito 11 - Crie uma funcao de Numero de Telefone

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,13 +11,55 @@ function techList(arrayTech, name) {
     }
     console.log(arrayTech2);
     return arrayTech2;
-  } 
+  }
   return 'Vazio!';
 }
 
+function restriction21(newZ) {
+  newZ.sort();
+  let arrayCount = [];
+  let count = 1;
+  for (let i = 0; i < newZ.length; i += 1) {
+    let nextNum = newZ[i + 1];
+    if (newZ[i] === nextNum) {
+      count += 1;
+    } else { arrayCount.push(count); count = 1; }
+  }
+  arrayCount.sort();
+  arrayCount.reverse();
+  return arrayCount;
+}
+
+function restriction2(z) {
+  let newZ = Array.from(z);
+  let arrayCount = restriction21(newZ);
+  if (arrayCount[0] > 2) {
+    return false;
+  }
+  return true;
+}
+
+function restriction1(z) {
+  for (let i = 0; i < z.length; i += 1) {
+    let actNumber = z[i];
+    if (actNumber > 9 || actNumber < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(z) {
+  let str1 = '';
+  if (z.length !== 11) {
+    str1 = 'Array com tamanho incorreto.';
+  } else if (restriction2(z) === false || restriction1(z) === false) {
+    str1 = 'não é possível gerar um número de telefone com esses valores';
+  } else {
+    str1 = `(${z[0]}${z[1]}) ${z[2]}${z[3]}${z[4]}${z[5]}${z[6]}-${z[7]}${z[8]}${z[9]}${z[10]}`;
+  }
+  return str1;
 }
 
 // Desafio 12
