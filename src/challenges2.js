5687abd6c71d022df1d8da5fb31998d474eee479
Morallys <morallys.juan@gmail.com>
2021-09-01 01:40:02 -0300

    Incluíndo alterações do arquivo challenges2.js com início do desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function criarObjeto(arrayTechs, nome) {
+  let ordemArray = arrayTechs.sort();
+  let conjuntoObj = [];
+
+  for (let index = 0; index < ordemArray.length; index += 1) {
+    conjuntoObj[index] = { tech: ordemArray[index], name: nome };
+  }
+  return conjuntoObj;
 }
+
+function techList(arrayTechs, name) {
+  if (arrayTechs.length > 0) {
+    console.log(criarObjeto(arrayTechs, name));
+  } else {
+    return 'Vazio!';
+  }
+}
+//console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
 function generatePhoneNumber() {
