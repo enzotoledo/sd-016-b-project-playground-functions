0039af4af96c8ce6632e644a75bee5bea125ae73
Gilson Alencar <gilson.alencar94@gmail.com>
2021-09-01 11:06:26 -0300

    Requirement 11 (bonus)

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 // Desafio 10
 function techList(techs, name) {
   let techObjects = [];
@@ -16,8 +17,37 @@ function techList(techs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// eslint-disable-next-line max-lines-per-function
+function generatePhoneNumber(numbers) {
+  let counter;
+  let errorMsg = 'não é possível gerar um número de telefone com esses valores';
+
+  if (numbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (const number of numbers) {
+    counter = 0;
+    if (number < 0) {
+      return errorMsg;
+    }
+    if (number > 9) {
+      return errorMsg;
+    }
+    for (let i = 0; i < numbers.length; i += 1) {
+      if (numbers[i] === number) {
+        counter += 1;
+      }
+    }
+    if (counter >= 3) {
+      return errorMsg;
+    }
+  }
+  let phoneNumber = numbers;
+  phoneNumber.splice(0, 0, '(');
+  phoneNumber.splice(3, 0, ')');
+  phoneNumber.splice(4, 0, ' ');
+  phoneNumber.splice(-4, 0, '-');
+  return phoneNumber.join('');
 }
 
 // Desafio 12
