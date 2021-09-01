950c802c849bfefc9f910eb9415f5c4a4734d2b2
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-08-31 22:07:45 -0300

    decimo primeiro requisito concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -22,9 +22,48 @@ function techList(list, name) {
   return result;
 }
 
+function verificaMaxMin(numbers) {
+  for (let number of numbers) {
+    if ((number < 0) || (number > 9)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function verificaRepeticao(numbers) {
+  for (let index = 0; index < numbers.length; index += 1) {
+    let count = 0;
+    for (let segundoIndex = index; segundoIndex < numbers.length; segundoIndex += 1) {
+      if (numbers[index] === numbers[segundoIndex]) {
+        count += 1;
+      }
+    }
+    if (count >= 3) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function mascaraTelefone(numbers) {
+  let phone = `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
+
+  return phone;
+}
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numbers) {
+  let retorno;
+  if (numbers.length !== 11) {
+    retorno = 'Array com tamanho incorreto.';
+  } else if ((verificaMaxMin(numbers)) || (verificaRepeticao(numbers))) {
+    retorno = 'não é possível gerar um número de telefone com esses valores';
+  } else {
+    retorno = mascaraTelefone(numbers);
+  }
+
+  return retorno;
 }
 
 // Desafio 12
