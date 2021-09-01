2d7f9b980cc7ece5c19e43de0eaa3651998d6f69
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-08-31 23:29:04 -0300

    decimo terceiro requisito pronto

--- src/challenges2.js
+++ src/challenges2.js
@@ -80,8 +80,25 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(value) {
+  let regex = /\d+/g;
+  let numeros = value.match(regex);
+  let resultado;
+
+  let sum = 0;
+  for (let num of numeros) {
+    sum += Number(num);
+  }
+
+  if (sum === 1) {
+    resultado = `${sum} copo de água`;
+  } else {
+    resultado = `${sum} copos de água`;
+  }
+  return resultado;
+
+  /* O código acima contem uma parte de um código que foi retirado do stackoverflow, feito pelo usuário bobo da corte 11-05-2015;
+  link: https://stackoverflow.com/questions/1623221/how-to-find-a-number-in-a-string-using-javascript/30160994 */
 }
 
 module.exports = {
