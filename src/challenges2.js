a11b3938311a780a7cd0d3d7de3691e0a4eac630
Fredi Wilkens <frediwilkens@gmail.com>
2021-08-31 21:57:23 -0300

    Ajustes no requisito 10 e conclusão do requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -10,7 +10,10 @@ function techList(array, name) {
     let list = [];
     //coloca cada um dos valores do array (já ordenados) na lista como objetos com as devidas propriedades. 
     for (let techs of organizedArray) {
-      list.push({ tech: techs, name: name });
+      list.push({
+        tech: techs,
+        name: name
+      });
     }
     return list
   }
@@ -18,8 +21,28 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+/*
+ * Consultei esse link para utilizar o template literals
+ * Link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Template_literals
+ */
+function generatePhoneNumber(array) {
+  if((array.length !== 11)) {
+    return 'Array com tamanho incorreto.';
+  } else {
+    let numberRepeat = 0;
+    for(let numbers of array) {
+      for (let index in array) {
+        if (numbers === array[index]){
+          numberRepeat += 1;
+        }
+        if ((numberRepeat >= 3) || (numbers > 10) || (numbers < 0)) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+      numberRepeat = 0;
+    }
+    return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+  }
 }
 
 // Desafio 12
