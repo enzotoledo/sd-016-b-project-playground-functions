fdced94f569256a8784ae3a64b9ece75e4ae5cc5
Gabriel Oliveira <gabrielfrox@gmail.com>
2021-09-01 00:45:19 -0300

    Desafio 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,23 @@
+// Criação do objeto
+function objectCreation(tecnologia, nome) {
+  let obj = {
+    tech: tecnologia,
+    name: nome,
+  };
+  return obj;
+}
+
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let objectList = [];
+  if (array.length === 0) {
+    return 'Vazio!';
+  }
+  let arrayOrdenado = array.sort();
+  for (let index = 0; index < arrayOrdenado.length; index += 1) {
+    objectList.push(objectCreation(arrayOrdenado[index], name));
+  }
+  return objectList;
 }
 
 // Desafio 11
