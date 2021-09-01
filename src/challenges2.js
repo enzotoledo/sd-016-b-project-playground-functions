1ae4bf9bdb6277c966ca726c82409ea94b05a652
Gabriel Pinheiro <g.felippe5965@gmail.com>
2021-08-31 23:24:54 -0300

    Exercicio 13 - concluído

--- src/challenges2.js
+++ src/challenges2.js
@@ -33,8 +33,20 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(str) {
+  let reg = /\d+/g;
+  let result = str.match(reg).map(Number);
+  let sum = 0;
+  let message;
+  for (let index = 0; index < result.length; index += 1) {
+    sum += result[index];
+  }
+  if (sum > 1) {
+    message = (`${+sum} copos de água`);
+  } else {
+    message = (`${+sum} copo de água`);
+  }
+  return message;
 }
 
 module.exports = {
