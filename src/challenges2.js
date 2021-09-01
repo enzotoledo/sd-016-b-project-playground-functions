745ee593ca00b9bdb9eb7cc5822d9e86801dd80c
Gabriel Pinheiro <g.felippe5965@gmail.com>
2021-09-01 00:00:44 -0300

    Exercicio 11 - Concluído.

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,28 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let str = array.toString().replace(/,/g, '');
+  let strTelephone = str.replace(/(\d{2})?(\d{5})?(\d{4})?/, '($1) $2-$3');
+  let count = 0;
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 0 || array[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let position = 0; position < array.length; position += 1) {
+      if (array[index] === array[position]) {
+        count = count + 1;
+      }
+    }
+    if (count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    count = 0;
+  }
+  return strTelephone;
 }
 
 // Desafio 12
@@ -33,6 +53,8 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
+// Reference (i): https://www.youtube.com/watch?v=pfkkdzeyx6U&ab_channel=AllThingsJavaScript%2CLLC
+// Reference (ii): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map?redirectlocale=en-US&redirectslug=JavaScript/Reference/Global_Objects/Array/map
 function hydrate(str) {
   let reg = /\d+/g;
   let result = str.match(reg).map(Number);
