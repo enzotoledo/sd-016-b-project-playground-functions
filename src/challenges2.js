f1f92f1ddd19560c8ed16383a1f03e08d962829b
Leonardo Henrique <leonardomachado@hotmail.co.uk>
2021-08-31 22:10:11 -0300

    Submete a resolucao do desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -11,13 +11,47 @@ function techList(array, name) {
     return newList;
 }
 
-
-
-
 // Desafio 11
-function generatePhoneNumber() {
-    // seu código aqui
+function generatePhoneNumber(array) {
+    let phoneNumber = '';
+    let ddd = '';
+    let first = '';
+    let second = '';
+
+
+    if (array.length !== 11) {
+        return 'Array com tamanho incorreto.'
+    }
+
+    for (index = 0; index < array.length; index++) {
+        let contador = 0;
+        for (b = 0; b < array.length; b++) {
+            if (array[index] === array[b]) {
+                contador++;
+            }
+        }
+        if (array[index] < 0 || array[index] > 9 || contador >= 3) {
+            return 'não é possível gerar um número de telefone com esses valores';
+
+        } else phoneNumber += array[index];
+    }
+
+    for (index = 0; index < 2; index++)
+        ddd += phoneNumber[index];
+
+    for (index = 2; index < 7; index++)
+        first += phoneNumber[index];
+
+    for (index = 7; index < 11; index++)
+        second += phoneNumber[index];
+
+
+
+
+    return '(' + ddd + ')' + ' ' + first + '-' + second;
+
 }
+
 
 // Desafio 12
 function triangleCheck() {
