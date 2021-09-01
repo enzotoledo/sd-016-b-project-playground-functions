e8a03d6ce9908ad734e52d2aae2c907eb8da6d54
Morallys <morallys.juan@gmail.com>
2021-09-01 05:29:00 -0300

    Andamento ao Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,12 +16,79 @@ function techList(arrayTechs, name) {
     return 'Vazio!';
   }
 }
-//console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function validaTamanho(arrayNumbers) {
+  if (arrayNumbers.length > 11 || arrayNumbers.length < 11) {
+    return false;
+  }
+  return true;
 }
+
+function validaNumero(arrayValido) {
+  for (let index = 0; index < arrayValido.length; index += 1) {
+    if (arrayValido[index] > 9 || arrayValido[index] < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function validaRepeticao(arrayValido) {
+  let aux = 1;
+
+  for (let countL = 0; countL < arrayValido.length; countL += 1) {
+    for (let countC = 0; countC < arrayValido.length; countC += 1) {
+      if (arrayValido[countL] === arrayValido[countC + 1]) {
+        aux += 1;
+        if (aux >= 3) {
+          return false;
+        }
+      }
+    }
+    aux = 0;
+  }
+  return true;
+}
+
+function impressaoRetornoTrue(retorno) {
+  let textAjust;
+
+  const part1 = retorno.slice(0, 2).join('');
+  const part2 = retorno.slice(2, 7).join('');
+  const part3 = retorno.slice(7, 11).join('');
+  textAjust = `(${part1}) ${part2}-${part3}`; // referência: https://www.alura.com.br/artigos/criando-uma-mascara-de-telefone-com-javascript
+
+  return textAjust;
+}
+
+function impressaoRetornoFalse(param) {
+  let aux = '';
+
+  if (param === 1) aux = 'Array com tamanho incorreto.';
+  else if (param === 2 || param === 3) {
+    aux = 'não é possível gerar um número de telefone com esses valores';
+  }
+  return aux;
+}
+
+function generatePhoneNumber(numbers) {
+  let contFuncao = 0;
+
+  if (validaTamanho(numbers) !== true) {
+    contFuncao = 1;
+    console.log(impressaoRetornoFalse(contFuncao));
+  } else if (validaNumero(numbers) !== true) {
+    contFuncao = 2;
+    console.log(impressaoRetornoFalse(contFuncao));
+  } else if (validaRepeticao(numbers) !== true) {
+    contFuncao = 3;
+    console.log(impressaoRetornoFalse(contFuncao));
+  } else {
+    console.log(impressaoRetornoTrue(numbers));
+  }
+}
+generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
 
 // Desafio 12
 function triangleCheck() {
