a9d13a21914e2e4880c33fed6e54aa5be36aa5d7
jon <jfs1barbosa@gmail.com>
2021-09-02 19:21:25 -0300

    correcoes

--- src/challenges.js
+++ src/challenges.js
@@ -19,7 +19,8 @@ function splitSentence(manga) {
 
 // Desafio 4
 function concatName(nameConcat) {
-  //
+  let namCon = 0;
+  return (nameConcat = nameConcat[nameConcat.length - 1] + ',' + nameConcat[0]);
 }
 
 // Desafio 5
@@ -28,25 +29,55 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  //
+function highestCount(maxNumber) {
+  let resultado = 0;
+  let maxRes;
+  for (let number of maxNumber) {
+    if (number === maxRes) {
+      result++;
+    } else if (!maxRes || number > maxNumber) {
+      maxRes = number;
+      resultado = 1;
+    }
+  }
+  return resultado;
 }
 
 // Desafio 7
-function catAndMouse() {
-  //
+function catAndMouse(mouse, catY, catX) {
+  if (Math.abs(mouse - catY) < Math.abs(mouse - catX)) {
+    return 'catY'
+  } else if (Math.abs(mouse - catX) < Math.abs(mouse - catY)){
+    return 'catX'
+  } else if (Math.abs(mouse - catY) === Math.abs(mouse - catX)) {
+    return 'os gatos se trombaram e o rato foge'
+  } 
 }
 
 // Desafio 8
 function fizzBuzz() {
-  //
+  if (buzzFizz % 3 === 0 && buzzFizz % 5 === 0) {
+    return 'fizzBuzz';
+  }
+  if (buzzFizz % 5 === 0) {
+    return 'buzz';
+  }
+  if (buzzFizz % 3 === 0) {
+    return 'fizz';
+  }
+  return 'bug!'
 }
 
 // Desafio 9
-function encode() {
-  //
+function encode(gocode) {
+  let gocode = { a: '1', e: '2', i: '3', o: '4', u: '5' };
+  let coded = gocode.replace(/[aeiou]/g, (letras) => gocode[letras]);
+  return coded;
 }
-function decode() {
+function decode(uncode) {
+  let uncode = { 1: 'a', 2: 'e', 3: 'i', 4: 'o', 5: 'u' };
+  let uncoded = uncode.replace(/[12345]/g, (letras) => uncoded[letras]);
+  return uncoded;
 }
 
 module.exports = {
