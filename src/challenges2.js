a7c128414c209b57f01c710427aff346895a359c
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-09-02 11:10:14 -0300

    Still trying challenge 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,22 +1,46 @@
 // Desafio 10
 function techList(tech, name) {
-
   const objectsList = [];
 
   if (tech.length === 0) {
-        return 'Vazio!';
- } 
-  tech.sort();
-  
-  for (let index = 0; index < tech.length; index += 1) {
-    objectsList.push({tech:tech[index], name: name})
+    return 'Vazio!';
   }
-  return objectsList
+  tech.sort();
+
+  for (let index = 0; index < tech.length; index += 1) {
+    objectsList.push({ tech: tech[index], name: name });
+  }
+  return objectsList;
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+
+
+function generatePhoneNumber(phoneNumber) {
+  let phonePattern = '';
+  let counter = 0;
+
+  if (phoneNumber.length !== 11) {
+    phonePattern = 'Array com tamanho incorreto.';
+  }
+  
+  for (let index = 0; index < phoneNumber.length; index += 1) {
+    if (phoneNumber[index] > 9 || phoneNumber[index] < 0){
+      phonePattern = 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let indexSecondLoop = 1; indexSecondLoop < phoneNumber.length;indexSecondLoop += 1) {
+      for (let indexThirdLoop = 2; indexThirdLoop < phoneNumber.length; indexThirdLoop += 1) {
+        if (phoneNumber[index] === phoneNumber[indexSecondLoop] === phoneNumber[indexThirdLoop]) {
+          counter += 1;
+          if (conter > 3) {
+            phonePattern = 'não é possível gerar um número de telefone com esses valores';
+          } else {
+            phonePattern = `(${phoneNumber.slice(0, 3)} 
+            ${phoneNumber.slice(3, 8)}-${phoneNumber.slice(8, 12)})`;}
+        }
+      }
+    }
+  return phonePattern;
 }
 
 // Desafio 12
