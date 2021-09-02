8f99d35cb8527b98432b2502d7184f1f6a3e2ee6
Hugo Maximiano <hugomaximiano@hotmail.com>
2021-09-02 19:55:27 -0300

    desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -40,28 +40,69 @@ let higthNumber = Math.max(numbers);
  }
 }
 
+/*
+  let higthNumber[0];
+  let counter = 0;
+
+  for (let index = 0; index < numbers.length; index +=1) {
+    if (numbers[index] > higthNumber) {
+      maiorNumero = arrayDeNumeros[index];
+    }
+    if (number[index] === higthNumber) {
+      counter += 1;
+    }
+  }
+  return counter;
+}
+*/
+
 // Desafio 7
 function catAndMouse(cat1, cat2, mouse) {
   if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
     return 'cat1';
-  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+  } 
+  else if(Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
     return 'cat2';
-  } else {
-    return "os gatos trombam e o rato foge";
+  } 
+  else if (Math.abs(cat1 - mouse) == Math.abs(cat2 - mouse)) {
+    return ('os gatos trombam e o rato foge');
   }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numbers) {
+  let mensager = []
+  for (let index = 0; index < numbers.length; index+=1) {
+    if (numbers[index]%3 === 0) {
+      return mensager.push ('fizz');
+    } else if (numbers[index]%5 === 0) {
+      return mensager.push('buzz');
+    } else if((numbers[index]%3 ) && (numbers[index]%5)) {
+      return mensager.push('fizzbuzz');
+    } else { 
+      return mensager.push ('bug!');
+    }
+  }    
 }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
+//if elseesafio 9
+function encode(msg) {
+ msg = msg.replace(/[a]/g, '1');
+ msg = msg.replace(/[e]/g, '2');
+ msg = msg.replace(/[i]/g, '3');
+ msg = msg.replace(/[o]/g, '4');
+ msg = msg.replace(/[u]/g, '5');
+return msg;
 }
+// Desafio concluido com dicas do Luiz Wanderson.
+
 function decode() {
-  // seu código aqui
+  msg = msg.replace(/[1]/g, 'a');
+  msg = msg.replace(/[2]/g, 'e');
+  msg = msg.replace(/[3]/g, 'i');
+  msg = msg.replace(/[4]/g, 'o');
+  msg = msg.replace(/[5]/g, 'u');
+ return msg;
 }
 
 module.exports = {
