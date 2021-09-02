f12d3fe80a5a3ffcc6de3c313a65ebfe5f74e617
MychaelJales <mychael_duarte@hotmail.com>
2021-09-01 23:00:31 -0300

    finaliza o desafio bonus 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,42 @@ function techList(arrayTechs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumbers) {
+  let phoneNumber = '';
+  let repeatTest = 0;
+  let repeatTest2 = false;
+  let numbersIr = false;
+
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    for (let index2 = 0; index2 < arrayNumbers.length; index2 += 1) {
+      if (arrayNumbers[index2] === arrayNumbers[index]) {
+        repeatTest += 1;
+      }
+    }
+
+    if (repeatTest >= 3) {
+      repeatTest2 = true;
+    }
+
+    if ((arrayNumbers[index] < 0) || (arrayNumbers[index] > 9)) {
+      numbersIr = true;
+    }
+
+    repeatTest = 0;
+  }
+
+  if (arrayNumbers.length !== 11) {
+    phoneNumber = 'Array com tamanho incorreto.';
+  } else if ((repeatTest2 === true) || (numbersIr === true)) {
+    phoneNumber = 'não é possível gerar um número de telefone com esses valores';
+  } else {
+    let DDD = ('(' + arrayNumbers[0] + arrayNumbers[1] + ')');
+    let fiveNumbers = ' ' + arrayNumbers[2] + arrayNumbers[3] + arrayNumbers[4] + arrayNumbers[5] + arrayNumbers[6];
+    let fourNumbers = '-' + arrayNumbers[7] + arrayNumbers[8] + arrayNumbers[9] + arrayNumbers[10];
+    phoneNumber = DDD + fiveNumbers + fourNumbers;
+  }
+
+  return phoneNumber;
 }
 
 // Desafio 12
