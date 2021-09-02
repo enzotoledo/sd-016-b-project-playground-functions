fc0fe23bc7de2cb99b03327dbf3ba315aeb9d588
jon <jfs1barbosa@gmail.com>
2021-09-02 19:32:18 -0300

    novos passos dados e correcao

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,29 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(nomes, tech) {
+  let resultado = [];
+  if (tech.length === 0) {
+    return 'vazia';
+  } else {
+    for (let tech of nomes.sort()) {
+      resultado.push({tech, name})
+    }
+    return resultado;
+  }
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(numero) {
+  if (Number.length === 11 && Number.length) {
+    let numPhone = '(' + number.slice(0, -9) + ')' + number.slice(2, -4) + '-' + number.slice(7);
+    let resultado = numPhone.replace(',', '');
+    return resultado;
+  } else if (number.length < 11) {
+    return 'array com tamanho errado';
+  }  else if (number < 0 && number > 9) {
+  } else {
+      return 'errado';
+    }
+  }
 
 // Desafio 12
 function triangleCheck() {
