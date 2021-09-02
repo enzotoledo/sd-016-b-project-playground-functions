b2a3d8911424798d859692bc2ba859c4ad0bbd99
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-01 22:53:24 -0300

    requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,25 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  let arrayOrdenada = tech.sort();
+  let array = [];
+
+  if (arrayOrdenada.length === 0) {
+    return 'Vazio!';
+  } else if (arrayOrdenada.length > 0) {
+    for(let index in arrayOrdenada) {
+      array.push({
+        tech: arrayOrdenada[index],
+        name: name,
+      })
+    }
+  }
+  return array;
 }
+console.log(techList(
+  ["React", "Jest", "HTML", "CSS", "JavaScript"],
+  "Lucas"));
+
 
 // Desafio 11
 function generatePhoneNumber() {
