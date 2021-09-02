9181270e9ad17deba72908592dd142a3c85db33a
lucaseix1 <lucas.bastos.seixas@gmail.com>
2021-09-02 18:44:22 -0300

    Disafio-07

--- src/challenges.js
+++ src/challenges.js
@@ -33,12 +33,18 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
+    return "cat1";
+  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)){
+    return "cat2";
+  } else {
+    return "os gatos trombam e o rato foge";
+  }
 }
 
 // Desafio 8
