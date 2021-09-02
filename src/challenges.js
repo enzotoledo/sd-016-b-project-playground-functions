2d70babe96dfb5860132170d3422c8f113cea59b
joao <joaocarlosbne@gmail.com>
2021-09-02 13:25:00 -0400

    Joao Carlos Iniciando o projeto

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,20 @@
 // Desafio 1
 function compareTrue() {
   // seu código aqui
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
+    return true;
+  }
+  return false;
 }
+console.log(compareTrue(true, false));
 
 // Desafio 2
 function calcArea() {
   // seu código aqui
+function calcArea(base, height) {
+  let areaTriangulo = (base * height) / 2;
+  return areaTriangulo;
 }
 
 // Desafio 3
