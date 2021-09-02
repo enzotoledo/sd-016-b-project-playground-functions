a6d23dd5bcf92daf69c2b041504c7fa5a1a27b6f
Felipe Sanches <sanchesmeirelles@gmail.com>
2021-09-01 21:06:51 -0300

    Exercicio 11 quase pronto

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,26 +19,25 @@ return answer;
 
 // Desafio 11
 function generatePhoneNumber(array) {
-let answer = [];
-function repeat (n) {
-  let nOrder = n.sort();
-  let count = 0;
-  let current = nOrder[0];
-  for (let i = 0; i < nOrder.length; i += 1) {
-    if (current === nOrder[(i + 1)]){
-      count += 1;
-    } else {
-      current = nOrder[(i + 1)]
+  function repeat (n) {
+    let nOrder = n.sort();
+    console.log(nOrder);
+    console.log(n);
+    let count = 0;
+    let current = nOrder[0];
+    for (let i = 0; i < nOrder.length; i += 1) {
+      if (current === nOrder[(i + 1)]){
+        count += 1;
+      } else {
+        current = nOrder[(i + 1)]
+      }
     }
-}
-if ( count >= 3){
-  return false;
-} else {
-  return true;
-}
-nOrder = n;
-}
-function verify (n2){ // Verificar se o max é maior que 9 e o min é menor que 0 ok
+    if ( count >= 3){
+      return false;
+    } else {
+      return true;
+    }
+  function verify (n2){ // Verificar se o max é maior que 9 e o min é menor que 0 ok
   let maxhNumber = Math.max.apply(null, n2);// Retirado a informação para fazer a linha do código no site 'https://pt.stackoverflow.com/questions/35202/como-saber-o-maior-valor-de-um-array'
   let minNumber = Math.min.apply(null, n2);// Retirado a informação para fazer a linha do código no site 'https://pt.stackoverflow.com/questions/35202/como-saber-o-maior-valor-de-um-array'
     if ((maxhNumber > 9) || (minNumber < 0)) {
@@ -46,13 +45,13 @@ function verify (n2){ // Verificar se o max é maior que 9 e o min é menor que 
     } else {
       return true;
     }
-
 }
 if ((array.length !== 11)) {
   return 'Array com tamanho incorreto.';
  } else if ((verify(array) === false ) || (repeat(array) === false)) {
   return 'não é possível gerar um número de telefone com esses valores';
  } else {
+  let answer = [];
 for (let indice = 0; indice < array.length; indice += 1 ){
   if (indice === 0){
     answer.push('(');
@@ -65,14 +64,15 @@ for (let indice = 0; indice < array.length; indice += 1 ){
     answer.push('-');
     answer.push(array[indice]);
   } else {
-    answer.push(array[indice])
+    answer.push(array[indice]);
   }
 }
 let resposta = answer.join('');
 return resposta;
-
  }
 }
+console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1]));
+
 
 // Desafio 12
 function triangleCheck() {
