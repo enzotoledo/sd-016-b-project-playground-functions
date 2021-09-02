4654ea6b4982de02211b298e41f8d67ad3818c5d
jon <jfs1barbosa@gmail.com>
2021-09-02 17:24:06 -0300

    refazendo

--- src/challenges.js
+++ src/challenges.js
@@ -19,7 +19,7 @@ function splitSentence(manga) {
 
 // Desafio 4
 function concatName(nameConcat) {
-  return nameConcat[nameConcat.length -1] + ', ' + nameConcat[0]
+  //
 }
 
 // Desafio 5
@@ -29,53 +29,24 @@ function footballPoints(wins, ties) {
 
 // Desafio 6
 function highestCount(maxNumber) {
-  let maxNumberA = maxNumber;
-  for (let i = 0; maxNumber.length; i += 1) {
-    for (let index = i + 1; maxNumberA.length; index += 1)
-      if (maxNumber[i] === maxNumberA[index]) {
-        maxNumber += 1;
-        return;
-      }
-  }
+  //
 }
 
 // Desafio 7
 function catAndMouse(mouse, catA, catB) {
-  if (Math.abs(mouse - catB) < Math.abs(mouse - catA)) {
-    return 'catB';
-  } else if (Math.abs(mouse - catA) < Math.abs(mouse - catB)) {
-    return 'catA';
-  } else if (Math.abs(mouse - catA) === Math.abs(mouse - catB)) {
-    return 'os gatos trombam e o rato foge';
-  } else {
-    return 'resultado não encontrado'
-  }
+  //
 }
 
 // Desafio 8
 function fizzBuzz(buzzFizz) {
-  for (index = 0; index < buzzFizz; index+= 1) {
-    if (buzzFizz % 3 === 0) {
-      return 'fizz';
-    } else if (buzzFizz % 5 === 0) {
-      return 'buzz'
-    } if (buzzFizz % 3 === 0 && buzzFizz % 5 === 0) {
-      return 'fizzBuzz';
-    } else {
-      return 'bug';
-    }
-  }
+  //
 }
 
 // Desafio 9
 function encode(codeY) {
-    let codeEncode = { a: '1', e: '2', i: '3', o: '4', u: '5' };
-    let codeYA = codeY.replace(/[aeiou]/g, (letras) => codeY[letras]);
-  
+  //
 }
 function decode(codeX) {
-  let codeXDecode = { 1: 'a', 2: 'e', 3: 'i', 4: 'o', 5: 'u' }
-  let codeXA = codeX.replace(/[12345]/g, (letras) => codeX[letras]);
 }
 
 module.exports = {
