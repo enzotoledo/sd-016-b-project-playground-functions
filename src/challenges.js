d42926288001b90da86a56f63727b8fabed45916
blmarquess <blmarques.dev@gmail.com>
2021-09-02 15:25:50 -0300

    estilizando code with EstLint

--- src/challenges.js
+++ src/challenges.js
@@ -1,26 +1,28 @@
+/* eslint-disable no-undef */
 // Desafio 1
 function compareTrue(value01, value02) {
   if (value01 === true && value02 === true) {
     return true;
-  } else { return false}
+  }
+  return false;
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  let triAng =  (base * height)/2;
+function calcArea(base, height) {
+  let triAng = (base * height) / 2;
   return triAng;
 }
 
 // Desafio 3
 function splitSentence(frase) {
-  let arrayResult = frase.split(" ");
+  let arrayResult = frase.split(' ');
   return arrayResult;
 }
 
 // Desafio 4
 function concatName(paLavras) {
-  return (paLavras.pop() +', '+ paLavras.shift());
-} 
+  return (`${paLavras.pop()}, ${paLavras.shift()}`);
+}
 
 // Desafio 5
 function footballPoints(wins, ties) {
@@ -33,12 +35,10 @@ function highestCount(listNumer) {
   // let listNumer = [0, 4, 4, 4, 9, 2, 1];
   let maiorNum = Math.max.apply(null, listNumer);
   let contador = 0;
-  // let maxRepit = listNumer.length;
-  // console.log(maxRepit);
-  // console.log(contMaior);
-  for (index = 0; index < listNumer.length; index +=1) {
+  for (index = 0; index < listNumer.length; index += 1) {
+    // eslint-disable-next-line no-undef
     if (maiorNum === listNumer[index]) {
-      contador +=1;
+      contador += 1;
     }
   }
   return contador;
@@ -49,17 +49,15 @@ function catAndMouse(mouse, cat1, cat2) {
   if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
     return 'cat1';
   }
-  else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+  if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
     return 'cat2';
-  } 
-  else {
-    return 'os gatos trombam e o rato foge';
-  }
+  } else {return 'os gatos trombam e o rato foge';
+ }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(numTree) {
+  // let fizboz = [];
 }
 
 // Desafio 9
