8478684f5230c4f7fa8439988bdacfe5ba2d20a8
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-09-03 10:55:17 -0300

    Still trying challenge 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,5 @@
 // Desafio 10
-function techList(tech, name) {
+function techList(tech, title) {
   const objectsList = [];
 
   if (tech.length === 0) {
@@ -8,31 +8,45 @@ function techList(tech, name) {
   tech.sort();
 
   for (let index = 0; index < tech.length; index += 1) {
-    objectsList.push({ tech: tech[index], name: name });
+    objectsList.push({ tech: tech[index], name: title });
   }
   return objectsList;
 }
 
-// Desafio 11
-function generatePhoneNumber() {
-  // let phonePattern = '';
-  // let counter = 0;
+function counting(number) {
+  let counter = 0;
+  for (let index = 0; index < number.length; index += 1) {
+    for (let indexSecondLoop = 0; indexSecondLoop < number.length; indexSecondLoop += 1) {
+      counter = 1;
+      if (number[index] === number[indexSecondLoop]) {
+        counter += 1;
+      }
+    }
+  }
+  return counter;
+}
 
-  // if (phoneNumber.length !== 11) {
-  //   phonePattern = 'Array com tamanho incorreto.';  // }
-  // for (let index = 0; index < phoneNumber.length; index += 1) {
-  //   let counter = 0;
-  //   for (let indexSecondLoop = 0; indexSecondLoop < phoneNumber.length; index += 1) {
-  //     counter = 1;
-  //     if (phoneNumber[index] === phoneNumber[indexSecondLoop]) {
-  //       counter += 1;  //     }
-  //     if (counter >= 3 || phoneNumber[index] > 9 || phoneNumber[index] < 0) {
-  //       phonePattern = 'não é possível gerar um número de telefone com esses valores';
-  //     } else {
-  //       phonePattern = `(${phoneNumber.slice(0, 3)} 
-  //       ${phoneNumber.slice(3, 8)}-${phoneNumber.slice(8, 12)})`}  //   }
-  // }
-  // return phonePattern;
+function turningNumbers(number) {
+  let newNumber = '';
+
+  newNumber += `(${number.slice(0, 2)}) ${number.slice(2, 7)}-${number.slice(7, 12)}`;
+
+  newNumber = newNumber.replace(/,/g, '');
+
+  return newNumber;
+}
+
+// Desafio 11
+function generatePhoneNumber(phoneNumber) {
+  if (phoneNumber.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < phoneNumber.length; index += 1) {
+    if (counting(phoneNumber) >= 3 || phoneNumber[index] > 9 || phoneNumber[index] < 0) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+      return turningNumbers(phoneNumber);
+    }
 }
 
 // Desafio 12
@@ -56,4 +70,4 @@ module.exports = {
   techList,
   hydrate,
   triangleCheck,
-}
+};
