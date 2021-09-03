d7d6ff6abf815985a5b1e4b4e33891ecba55662a
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-02 23:31:53 -0300

    termina exercicio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,14 +3,54 @@ function techList(newTechs, name) {
   
 }
 
+function verificaDuplicados(numero, arrayTel){
+
+  let cont = 0;
+
+  for(let num of arrayTel){
+
+    if(num === numero){
+      cont++;
+    }
+
+  }
+
+  return cont;
+
+}
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayTel) {
+
+  if(arrayTel.length != 11){
+    return "Array com tamanho incorreto."
+  }
+
+  for(let i = 0 ; i < arrayTel.length ; i++){
+
+    if((arrayTel[i]<0) || (arrayTel[i]>9)){
+      return "não é possível gerar um número de telefone com esses valores"
+     } 
+
+    if(verificaDuplicados(arrayTel[i], arrayTel) === 3){
+      return "não é possível gerar um número de telefone com esses valores"
+     }
+     
+      
+  }
+  
+  
+  return "("+arrayTel[0]+arrayTel[1]+")"+" "+arrayTel[2]+arrayTel[3]+arrayTel[4]+arrayTel[5]+arrayTel[6]+"-"+arrayTel[7]+arrayTel[8]+arrayTel[9]+arrayTel[10]
+
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA,lineB,lineC) {
+
+  if((lineA > (lineB+LineC)) || (lineB > (lineA+LineC))){
+
+  }
+
 }
 
 // Desafio 13
