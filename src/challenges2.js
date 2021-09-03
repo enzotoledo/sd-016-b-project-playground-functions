7e52ddc43273f3676af3f065f6f1de6ca127e164
Pedro Fonseca <pedrinhopedro01@gmail.com>
2021-09-03 14:46:19 -0300

    Exercicio 11 resolvido, mas nao passou no lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,26 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } else if (array.length === 11) {
+    for (let i = 0; i < array.length; i++) {
+      let counter = 0;
+      if (array[i] < 0 || array[i] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores'
+      }
+      for (let j in array) {
+        if (array[i] === array[j]) {
+          counter++
+          if (counter > 2) {
+            return 'não é possível gerar um número de telefone com esses valores'
+          }
+        }
+      }
+    }
+  }
+  return `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
 }
 
 // Desafio 12
