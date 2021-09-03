84d751bec920857502e8a342e8dc01c791df3468
AmandaSottani <amandasottani@gmail.com>
2021-09-03 17:27:50 -0300

    exercicio 8 concluido e correção do quatro

--- src/challenges.js
+++ src/challenges.js
@@ -49,13 +49,37 @@ function highestCount(numeros) {
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
- 
+ let distanciaCat1 = Math.abs(cat1-mouse);
+ let distanciaCat2 = Math.abs(cat2-mouse);
+  if(distanciaCat1>distanciaCat2) {
+    return "cat1"; 
+  }else if(distanciaCat2>distanciaCat1) {
+    return "cat2";
+  }else {
+    return "os gatos trombam e o rato foge";
+  } 
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(lista) {
+  let array = lista.split(",");
+  console.log(array);
+  let novaArray = [];
+  for(let i=0; i<array.length; i++) {
+    if(array[i]%3==0 && array[i]%5==0){
+      novaArray[i] = "fizzbuzz";
+    }else if(array[i]%3==0){
+      novaArray[i] = "fizz";
+    }else if(array[i]%5==0){
+      novaArray[i] = "buzz";
+    }else{
+      novaArray[i] = "bug!";
+    }
+    
+  }
+  console.log(novaArray);
 }
+fizzBuzz ([2, 15, 7, 9, 45])
 
 // Desafio 9
 function encode() {
