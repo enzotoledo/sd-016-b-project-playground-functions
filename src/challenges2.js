dac61b6d8a174638b011bc036d17bf225b20d126
Douglas Sabino <douglasbsabino@hotmail.com>
2021-09-03 12:58:08 -0300

    termina exercicio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -58,8 +58,60 @@ function triangleCheck(lineA,lineB,lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+
+  let arrayResposta = string.split('');
+  let soma = 0;
+  
+  for(let i = 0 ; i < string.length ; i++){
+    
+    switch(string[i]){
+
+      case '1':
+        soma = soma + 1;
+        break;
+
+      case '2':
+        soma = soma + 2;
+        break;
+
+      case '3':
+        soma = soma + 3;
+        break;
+        
+      case '4':
+        soma = soma + 4;
+        break;
+        
+      case '5':
+        soma = soma + 5;
+        break;
+        
+      case '6':
+        soma = soma + 6;
+        break;
+        
+      case '7':
+        soma = soma + 7;
+        break;
+        
+      case '8':
+        soma = soma + 8;
+        break;
+        
+      case '9':
+        soma = soma + 9;
+        break;  
+    }
+
+  }
+
+  if(soma === 1){
+    return soma + " copo de água";
+  }else{
+    return soma + " copos de água";
+  } 
+
 }
 
 module.exports = {
