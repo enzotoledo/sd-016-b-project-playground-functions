6cab70b7c5a9c1929642655cc16415f71b331cf4
victor_calidor <victor.calidor@gmail.com>
2021-09-03 14:07:05 -0300

    Challenge 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,7 +6,7 @@ function techList(array, name) {
   }
   let newArray = array.sort();
   for (let i = 0; i < array.length; i += 1) {
-    objeto[i] = { 'tech': newArray[i], 'name': name };
+    objeto[i] = { tech: newArray[i], name };
   }
   return Object.getOwnPropertyNames(objeto);
 }
@@ -14,6 +14,7 @@ function techList(array, name) {
 // Desafio 11
 function generatePhoneNumber(array) {
   let number = [];
+  let repeticoes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   if (array.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
@@ -21,11 +22,19 @@ function generatePhoneNumber(array) {
     if (array[i] > 9 || array[i] < 0) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
+
+    repeticoes[array[i]]++;
     number.push(array[i]);
   }
-  return number;
+  for (let i = 0; i < repeticoes.length; i += 1) {
+    if (repeticoes[i] >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  console.log(repeticoes)
+  let numberArrumado = `(${number[0].toString() + number[1].toString()}) ${number[2].toString() + number[3].toString() + number[4].toString() + number[5].toString() + number[6].toString()}-${number[7].toString() + number[8].toString() + number[9].toString() + number[10].toString()}`;
+  return numberArrumado;
 }
-
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (lineA + lineB > lineC && lineC > Math.abs(lineA - lineB)) {
