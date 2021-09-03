4af0ac2930cfb2685dac9decbf7e0beca47eb185
Kevin Sena <senakevin50@gmail.com>
2021-09-03 19:02:05 -0300

    Task 11 completed

--- src/challenges2.js
+++ src/challenges2.js
@@ -20,8 +20,51 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  let error = 0
+  let numStructure = ['(','','',')',' ','','','','','','-','','','','',]
+
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.'
+  }
+
+  function errorMsg() {
+    return 'não é possível gerar um número de telefone com esses valores'
+  }
+
+  for (const iterator of array) {
+    let count = 0
+    if (iterator < 0 || iterator > 9) {
+      error += 1
+    }
+    for (let i = 0; i < array.length; i += 1) {
+      if (iterator === array[i]) {
+        count += 1
+      }
+      if (count >= 3) {
+        error += 1
+      }
+    }
+  }
+
+  function number(param) {
+    for (const iterator of param) {
+      for (let i = 0; i < numStructure.length; i += 1) {
+        if (numStructure[i] === '') {
+          numStructure[i] = iterator
+          break
+        }
+      }
+    }
+    return numStructure
+  }
+
+  if (error > 0) {
+    return errorMsg()
+  }
+
+  return number(array).join('')
 }
 
 // Desafio 12
