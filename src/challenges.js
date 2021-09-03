83cfc3512d128114b1ccf03b34d6f66fa6c6ef8f
Seu nome <s.drksilva@gmail.com>
2021-09-03 13:29:13 -0300

    Exercices 1 ~ 5 pass

--- src/challenges.js
+++ src/challenges.js
@@ -1,41 +1,56 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(v, k) {
   // seu código aqui
+  if (v == true && k == true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(b, h) {
   // seu código aqui
+  return b * h / 2;
 }
 
 // Desafio 3
-function splitSentence() {
+function splitSentence(s) {
   // seu código aqui
+  return s.split(" ");
 }
 
 // Desafio 4
-function concatName() {
+function concatName(v) {
   // seu código aqui
+  return v[v.length - 1].concat(", ", v[0]);
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return (3 * wins) + ties;
 }
 
 // Desafio 6
-function highestCount() {
+function highestCount(value) {
   // seu código aqui
+  
 }
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+  let m = Math.abs(mouse),
+      c1 = Math.abs(cat1),
+      c2 = Math.abs(cat2);
+  
 }
 
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(value) {
   // seu código aqui
+  
 }
 
 // Desafio 9
