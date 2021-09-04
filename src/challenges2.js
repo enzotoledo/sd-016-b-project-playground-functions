68221e11f8ffd05ceb069912145c862b0a629749
Taina <tainnaps@gmail.com>
2021-09-04 14:21:14 -0300

    Requisito 11 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,8 +17,48 @@ function techList(technologies, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function elementIsRepeated(numbers) {
+  let counter = 0;
+
+  for (let index = 0; index < numbers.length; index += 1) {
+    for (let element of numbers) {
+      if (element === numbers[index]) {
+        counter += 1;
+        if (counter >= 3) {
+          return true;
+        }
+      }
+    }
+    counter = 0;
+  }
+}
+function elementIsOutOfRange(numbers) {
+  for (let element of numbers) {
+    if (element < 0 || element > 9) {
+      return true;
+    }
+  }
+}
+function sizeIsOutOfRang(numbers) {
+  if (numbers.length !== 11) {
+    return true;
+  }
+}
+function putPhoneNumberElements(numbers) {
+  return `(${numbers[0]}${numbers[1]}) ${numbers[2]}${numbers[3]}${numbers[4]}${numbers[5]}${numbers[6]}-${numbers[7]}${numbers[8]}${numbers[9]}${numbers[10]}`;
+}
+function generatePhoneNumber(numbers) {
+  if (sizeIsOutOfRang(numbers)) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (elementIsRepeated(numbers) || elementIsOutOfRange(numbers)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+
+  let phoneNumber = putPhoneNumberElements(numbers);
+
+  return phoneNumber;
 }
 
 // Desafio 12
