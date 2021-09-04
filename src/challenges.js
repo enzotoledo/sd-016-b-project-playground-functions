e5b28399955e812295dbdc5a291b6665149bcf61
Lucas Bertarelli <lucas.bertarelli@gmail.com>
2021-09-03 22:30:36 -0300

    finaliza requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -62,7 +62,7 @@ function fizzBuzz(numbersArray) {
   let wordsArray = [];
   for (let index = 0; index < numbersArray.length; index += 1) {
     if (numbersArray[index] % 3 === 0 && numbersArray[index] % 5 === 0) {
-    wordsArray.push("fizzBuzz");
+      wordsArray.push("fizzBuzz");
     } else if (numbersArray[index] % 5 === 0) {
       wordsArray.push("buzz");
     } else if (numbersArray[index] % 3 === 0) {
@@ -75,11 +75,38 @@ function fizzBuzz(numbersArray) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+  function encode(phrase1) {
+  for (let index = 0; index < phrase1.length; index += 1) {
+    if (phrase1[index] === "a") {
+      phrase1 = phrase1.replace("a", 1);
+    } else if (phrase1[index] === "e") {
+      phrase1 = phrase1.replace("e", 2);
+    } else if (phrase1[index] === "i") {
+      phrase1 = phrase1.replace("i", 3);
+    } else if (phrase1[index] === "o") {
+      phrase1 = phrase1.replace("o", 4);
+    } else if (phrase1[index] === "u") {
+      phrase1 = phrase1.replace("u", 5);
+    }
+  }
+  return phrase1
 }
-function decode() {
-  // seu código aqui
+
+function decode(phrase2) {
+  for (let index = 0; index < phrase2.length; index += 1) {
+    if (phrase2[index] === "1") {
+      phrase2 = phrase2.replace("1", "a");
+    } else if (phrase2[index] === "2") {
+      phrase2 = phrase2.replace("2", "e");
+    } else if (phrase2[index] === "3") {
+      phrase2 = phrase2.replace("3", "i");
+    } else if (phrase2[index] === "4") {
+      phrase2 = phrase2.replace("4", "o");
+    } else if (phrase2[index] === "5") {
+      phrase2 = phrase2.replace("5", "u");
+    }
+  }
+  return phrase2
 }
 
 module.exports = {
