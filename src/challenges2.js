4903b2e01cf49a3c928c195ea2d14d908a478f68
Alissandra <alissandraraysa@gmail.com>
2021-09-04 18:21:03 -0300

    atualiza desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -65,7 +65,7 @@ function generatePhoneNumber(numbers) {
   }
 
 }
-console.log(generatePhoneNumber (numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+//console.log(generatePhoneNumber (numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
