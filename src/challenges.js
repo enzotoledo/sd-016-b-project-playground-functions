41fa2e7955abf07a941597f73a9db3511cb80f37
Juliana Miranda <juliana.ms@live.com>
2021-09-03 21:23:22 -0300

    Desafio 9

--- src/challenges.js
+++ src/challenges.js
@@ -61,7 +61,6 @@ function catAndMouse(mouse, cat1, cat2) {
 
 // Desafio 8
 /* A função abaixo faz parte da resolução do Desafio 8, foi feita para reduzir a complexidade da function exigida pelo lint na resolução principal do desafio proposto. */
-
 function resultadoFinal(numAtual, arrayFinal) {
   if (Math.trunc(numAtual % 3) === 0 && Math.trunc(numAtual % 5) === 0) {
     arrayFinal.push('fizzBuzz');
@@ -73,7 +72,6 @@ function resultadoFinal(numAtual, arrayFinal) {
     arrayFinal.push('bug!');
   }
 }
-
 function fizzBuzz(arrayInicial) {
   let resultado = [];
   for (let i = 0; i < arrayInicial.length; i += 1) {
@@ -83,11 +81,51 @@ function fizzBuzz(arrayInicial) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function letraPorNumero(index, arrayEncode) {
+  let codigo = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  for (let key in codigo) {
+    if (key === arrayEncode[index]) {
+      arrayEncode[index] = codigo[key];
+    }
+  }
+  return arrayEncode[index];
 }
-function decode() {
-  // seu código aqui
+function numeroPorLetra(index, arrayDecode) {
+  let codigo2 = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  for (let key in codigo2) {
+    if (key === arrayDecode[index]) {
+      arrayDecode[index] = codigo2[key];
+    }
+  }
+  return arrayDecode;
+}
+function encode(stringNormal) {
+  let arrayE = stringNormal.split('');
+  for (let i = 0; i < arrayE.length; i += 1) {
+    letraPorNumero(i, arrayE);
+  }
+  let frase = arrayE.join('');
+  return frase;
+}
+function decode(stringNormal) {
+  let arrayD = stringNormal.split('');
+  for (let x = 0; x < arrayD.length; x += 1) {
+    numeroPorLetra(x, arrayD);
+  }
+  let frase = arrayD.join('');
+  return frase;
 }
 
 module.exports = {
