428f41a074d92f7455bdb0220471870cd2e76368
Celso Fiorani <celsofiorani@gmail.com>
2021-09-05 00:11:32 -0300

    parte 8

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
-function compareTrue(comp1,comp2) {
-  if (comp1===true && comp2 ===true) {
+function compareTrue(comp1, comp2) {
+  if (comp1 === true && comp2 === true) {
     return true
   } else {
     return false
@@ -8,41 +8,41 @@ function compareTrue(comp1,comp2) {
 }
 
 // Desafio 2
-function calcArea(base,height) {
-  return ((base*height)/2)
+function calcArea(base, height) {
+  return ((base * height) / 2)
 }
 
 // Desafio 3
 function splitSentence(arr) {
-  return arr.split (" ")
+  return arr.split(" ")
 }
 
 // Desafio 4
 function concatName(string) {
- return string[string.length -1]+", "+ string [0]; 
-} 
+  return string[string.length - 1] + ", " + string[0];
+}
 
 // Desafio 5
-function footballPoints(wins,ties) {
-  return wins*3 + ties
+function footballPoints(wins, ties) {
+  return wins * 3 + ties
 }
 
 // Desafio 6
 function highestCount(param1) {
-  let maiorNumero =param1[0];
+  let maiorNumero = param1[0];
   let qntsRepete = 0;
 
-for (let i = 1; i  <param1.length; i+=1) {
-  if (param1[i]>maiorNumero) {
-    maiorNumero = param1[i]
+  for (let i = 1; i < param1.length; i += 1) {
+    if (param1[i] > maiorNumero) {
+      maiorNumero = param1[i]
+    }
   }
-}
-for (let index = 0; index < param1.length; index+=1){
-  if (param1[index] === maiorNumero) {
-    qntsRepete += 1;
+  for (let index = 0; index < param1.length; index += 1) {
+    if (param1[index] === maiorNumero) {
+      qntsRepete += 1;
+    }
   }
-}
-return qntsRepete;
+  return qntsRepete;
 }
 
 // Desafio 7
@@ -59,17 +59,35 @@ function catAndMouse(mouse, cat1, cat2) {
     return param3;
   }
 }
-console.log(catAndMouse(0, 4, 3))
+console.log(catAndMouse(0, 4, 3));
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(param1) {
+  let div3 = 'fizz';
+  let div5 = 'buzz';
+  let div35 = 'fizzBuzz';
+  let bug = 'bug!';
+
+  for (let index = 0; index < param1.length; index += 1) {
+    if (param1[index] % 3 === 0 && param1[index] % 5 === 0) {
+      param1[index] = div35;
+    } else if (param1[index] % 3 === 0) {
+      param1[index] = div3;
+    } else if (param1[index] % 5 === 0) {
+      param1[index] = div5;
+    } else {
+      param1[index] = bug;
+    }
+  }
+  return param1
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]))
 
 // Desafio 9
 function encode() {
   // seu código aqui
 }
+
 function decode() {
   // seu código aqui
 }
