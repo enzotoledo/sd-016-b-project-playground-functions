f9be623878e11a89b42719b6fbb4121d4e64db08
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-09-04 23:01:04 -0300

    passo 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,10 +1,10 @@
 // Desafio 10
 function techList(techs, nome) {
-  //cria uum funcao de listas de tec recebendo um array de nomes
-  //Deve tqambém receber um segundo parametro name
-  //para cada tec no array criar um objeto
-  //os objetos devem ser inseridos em uma lista em ordem crescente a partir do campo tech no objeto.
-  //saida da função é uma lista de objetos ordenada pelo campo tech dos objs formados
+  // cria uum funcao de listas de tec recebendo um array de nomes
+  // Deve tqambém receber um segundo parametro name
+  // para cada tec no array criar um objeto
+  // os objetos devem ser inseridos em uma lista em ordem crescente a partir do campo tech no objeto.
+  // saida da função é uma lista de objetos ordenada pelo campo tech dos objs formados
   // caso o array venha vazio a função deve retornar 'Vazio!'
 
   if (techs.length === 0) {
@@ -25,13 +25,25 @@ function techList(techs, nome) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeroTefone) {
+  // recebe uma array c 11 numeros 
+  // retornar um numero de telefone respeitando parenteses, traços e espaços
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  // retornar se é possivel formar um triangulo com os valores das linhas que
+
+  if (lineA < lineB + lineC && lineA > Math.abs((lineB - lineC))) {
+    return true;
+  }
+  if (lineB < lineA + lineC && lineB > Math.abs((lineA - lineC))) {
+    return true;
+  }
+  // if (lineB < lineA + lineC && lineB > Math.abs((lineA - lineC))) {
+  //   return true;
+  // }
+  return false;
 }
 
 // Desafio 13
