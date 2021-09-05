ed0d00a71fe700513fc210416d597ad6df58a66d
Danielle Silva <danielle.luisasilva@gmail.com>
2021-09-05 19:16:36 -0300

    adiciona requisito 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -21,10 +21,34 @@ console.log(techList(
   "Lucas"));
 
 
+  
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(numeros) {
   // seu código aqui
+  // let numeroQueMaisAparece = 0;
+  // let vezesQueAparece = 0;
+
+  // for (let index = 0; index < numeros.length; index += 1) {
+  //   if (numeros[index] === numeros[index]) {
+  //       numeroQueMaisAparece = numeros[index];
+  //   }
+  // }
+  // for (let i = 0; i < numeros.length; i += 1) {
+  //   if (numeros[i] === numeroQueMaisAparece) {
+  //       vezesQueAparece += 1;
+  //   }
+  // }
+ 
+  // for (let index =0; index < numeros.length; index += 1) {
+  //   if (numeros[index] > 9 || numeros[index] < 0 || vezesQueAparece >= 3) {
+  //     return 'não é possível gerar um número de telefone com esses valores';
+  //   } else if (numeros.length !== 11) {
+  //     return 'Array com tamanho incorreto.';
+  //   } 
+  // }
+
 }
+console.log(generatePhoneNumber([1, 2, 2, 4, 5, 5, 2, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck(valor1, valor2, valor3) {
@@ -42,16 +66,28 @@ function triangleCheck(valor1, valor2, valor3) {
   } else if (valor3 < valor1 + valor2) {
     return true;
   }
-  
-  
-   
 }
 console.log(triangleCheck(10, 13, 2));
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(stringBebidas) {
+  let testando = stringBebidas.split();
+  let resultadoo = testando[0].match(/\d+/gi).map(Number);
+  let coposDeAgua = 0;
+
+  for (let index = 0; index < resultadoo.length; index += 1) {
+    coposDeAgua += resultadoo[index];
+  }
+
+  if (coposDeAgua <= 1) {
+      return coposDeAgua + ' copo de água';
+    } else if (coposDeAgua >= 2) {
+      return coposDeAgua + ' copos de água';
+    }
+  
+  
 }
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
