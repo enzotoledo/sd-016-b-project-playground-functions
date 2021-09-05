723b54907c65da191d0432237ca190f8f171f795
nathaliamiranda <mirandanathaliaf@gmail.com>
2021-09-04 21:57:09 -0300

    passo 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,27 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(techs, nome) {
+  //cria uum funcao de listas de tec recebendo um array de nomes
+  //Deve tqambém receber um segundo parametro name
+  //para cada tec no array criar um objeto
+  //os objetos devem ser inseridos em uma lista em ordem crescente a partir do campo tech no objeto.
+  //saida da função é uma lista de objetos ordenada pelo campo tech dos objs formados
+  // caso o array venha vazio a função deve retornar 'Vazio!'
+
+  if (techs.length === 0) {
+    return 'Vazio!';
+  }
+
+  let tecnologias = [];
+  techs = techs.sort();
+  for (let tec of techs) {
+    let objeto = {
+      tech: tec,
+      name: nome
+    };
+    tecnologias.push(objeto);
+  }
+  return tecnologias;
+
 }
 
 // Desafio 11
