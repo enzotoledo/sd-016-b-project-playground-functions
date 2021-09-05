6dcc504e28fdb6d2bd3b68c09461de6fbee681c3
Flavio Alves <flavio.alves84@gmail.com>
2021-09-05 16:23:42 -0300

    Requerimento 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,18 @@
 // Desafio 10
-function techList() {
+function techList(tech, name) {
   // seu código aqui
+  let technoList = [];
+  let objectList = tech.sort();
+  if (objectList.length === 0) {
+    return 'Vazio!';
+  }
+  for (let x in objectList) {
+    technoList.push({
+      tech: objectList[x],
+      name,
+    });
+  }
+  return technoList;
 }
 
 // Desafio 11
