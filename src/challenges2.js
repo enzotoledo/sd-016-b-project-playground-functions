f3a64f273f2325fae474a4609919d7c6155adefa
Nathalia <nathaliamagi@gmail.com>
2021-09-05 11:42:40 -0300

    Challenges2

--- src/challenges2.js
+++ src/challenges2.js
@@ -2,13 +2,13 @@
 function techList(arrayTec, name) {
   let listaTecnologia = [];
   let array = arrayTec.sort();
-
-  if(array.length === 0) {
+  
+  if (array.length === 0) {
     return 'Vazio!';
   }
-for(let index in array) {
-    listaTecnologia.push({
-      tech : array[index],
+  for (let index in array) {
+    listaTecnologia.push( {
+      tech: array[index],
       name, 
     });
   }
@@ -16,27 +16,24 @@ for(let index in array) {
 }
 // console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'],"Lucas"));
 
-
 // Desafio 11
-
-
 function generatePhoneNumber(arrayPhone) {
   let qtdRepete;
 
   if (arrayPhone.length !== 11) {
     return 'Array com tamanho incorreto.';
   }
-  for(let index of arrayPhone) {// Para fazer a comparação foi preciso colocar um for dentro de outro 
+  for (let index of arrayPhone) {// Para fazer a comparação foi preciso colocar um for dentro de outro 
     qtdRepete = 0; // A variável não inicializada zera aqui e não permite que o for seja realizado de novo após passar pelos ifs anteriores
 
-    if(index < 0 || index > 9) {
+    if (index < 0 || index > 9) {
       return 'não é possível gerar um número de telefone com esses valores';
     }
-    for(let index2 of arrayPhone) {
+    for (let index2 of arrayPhone) {
       if(index2 === index) {
         qtdRepete += 1;
        }
-      if(qtdRepete >= 3) {
+      if (qtdRepete >= 3) {
         return 'não é possível gerar um número de telefone com esses valores';
       }
     }
@@ -50,18 +47,16 @@ function generatePhoneNumber(arrayPhone) {
   return phone.join('');
 }
  
-// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
+ // console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
-// Desafio 12
-
-function triangleCheck(lineA, lineB, lineC) {
-  let a;
-  let b;
-  let c;
+ // Desafio 12
+ function triangleCheck(lineA, lineB, lineC) {
+  let a, b, c;
   let retornar;
   let result = a + b + c;
+
   if ((lineA < lineB + lineC) && (lineA > Math.abs(lineB - lineC))) {
-    retornar = (a = true)
+    retornar = (a = true);
     return retornar;
   } 
   if ((lineA > lineB + lineC) && (lineA > Math.abs(lineB - lineC))) {
@@ -78,7 +73,7 @@ function triangleCheck(lineA, lineB, lineC) {
   }
   if (lineC < lineA + lineB && lineC < Math.abs(lineA - lineB)) {
     retornar = (c = true);
-    return retornar
+    return retornar;
   }
   if (lineC < lineA + lineB && lineC > Math.abs(lineA - lineB)) {
     retornar = (c = false);
@@ -88,8 +83,8 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 
-// Desafio 13
-
+ // Desafio 13
+ 
 function hydrate(string) {
   // Para extrair o número de uma string é preciso usar do método match
   // que é uma expressão regular que determnina a busca por dígitos em uma frase
@@ -102,12 +97,12 @@ function hydrate(string) {
   let arrayInt = [];
   let sum = 0;
 
-  for(let index = 0 ; index < regString.length ; index += 1) {
+  for (let index = 0 ; index < regString.length ; index += 1) {
     regInt = parseInt(regString[index]);
     arrayInt.push(regInt);
     sum += arrayInt[index];
   }
-  if(sum === 1) {
+  if (sum === 1) {
     return (sum + ' copo de água');
   } else {
     return (sum + ' copos de água');
