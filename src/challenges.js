d96851704208805ce490a0490afd891f3dc8be82
ErickVianaC <erickcosta535@gmail.com>
2021-09-05 22:19:02 -0300

    Refiz r1

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,11 @@
 // Desafio 1
 function compareTrue(value1, value2) {
-  if ((value1 == true) && (value2 == true))
+  if (value1 && value2 === true)
     return true; {
     return false
   }
 }
+
 console.log(compareTrue(true, false));
 
 // Desafio 2
