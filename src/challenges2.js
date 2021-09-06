5b7b33c2689e0c8b11ffd81127f8dd589c19d1ea
Eduardo Fradique <74576664+edsfradique@users.noreply.github.com>
2021-09-06 14:35:13 -0300

    C12 - triangleCheck

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,8 +9,9 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if ((lineA < (lineB + lineC)) && (lineA > Math.abs(lineB - lineC))) return true;
+  return false;
 }
 
 // Desafio 13
