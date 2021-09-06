828bfd02fb415c99260639e0b80712f54e96960b
Rafael Catarino <rafaelscatarino@hotmail.com>
2021-09-06 00:57:29 -0300

    adiciona exercicio

--- src/challenges2.js
+++ src/challenges2.js
@@ -28,11 +28,46 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-
-}
-
-hydrate('1 cachaça, 5 cervejas e 1 copo de vinho');
+function hydrate(param) {
+  let character = param.split('');
+  let cont = 0;
+  for (let value of character) {
+    switch (value) {
+      case '1':
+        cont += 1;
+        break;
+      case '2':
+        cont +=2;
+        break;
+      case '3':
+        cont +=3;
+        break;
+      case '4':
+        cont += 4;
+        break;
+      case '5':
+        cont += 5;
+        break;
+      case '6':
+        cont += 6;
+        break;
+      case '7':
+        cont += 7;
+        break;
+      case '8':
+        cont += 8;
+        break;
+      case '9':
+        cont += 9;
+        break;
+    }
+  }
+  if (cont === 1){
+    return cont +' copo de água';
+  } else {
+    return cont + ' copos de água';
+  }
+  }
 
 module.exports = {
   generatePhoneNumber,
