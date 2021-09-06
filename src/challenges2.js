8b2fd0366c6105668c7dad9557eca458aaf36242
Bruno Matumoto <brunomatumoto@MacBook-Air-de-Bruno.local>
2021-09-06 16:54:06 -0300

    Add function generatePhoneNumber completed

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,9 +16,46 @@ function techList(itemsTech, name) {
   return arrayTech;
 }
 
+function checkPhoneNumber(numberReference, arrayPhone) {
+  let count = 0;
+  for (let number of arrayPhone) {
+    if (number === numberReference) {
+      count += 1;
+    }
+    if (count >= 3) {
+      break;
+    }
+  }
+  return count;
+}
+
+function validPhoneNumber(arrayPhone) {
+  let repeatNumber;
+  let validPhone = true;
+  for (let number of arrayPhone) {
+    repeatNumber = checkPhoneNumber(number, arrayPhone);
+    if (number < 0 || number > 9 || repeatNumber >= 3) {
+      validPhone = false;
+    }
+  }
+  return validPhone;
+}
+
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arrPhone) {
   // seu código aqui
+  if (arrPhone.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let telephoneNumber = validPhoneNumber(arrPhone);
+  if (telephoneNumber) {
+    let prefix = arrPhone.slice(0, 2);
+    let phonePart1 = arrPhone.slice(2, 7);
+    let phonePart2 = arrPhone.slice(7, 11);
+    let concatPhoneNumber = `(${prefix.join('')}) ${phonePart1.join('')}-${phonePart2.join('')}`;
+    return concatPhoneNumber;
+  }
+  return 'não é possível gerar um número de telefone com esses valores';
 }
 
 // Desafio 12
