2441977c2dd77fc29b735add8da4f77f4d6ecd65
brunocsrribeiro <ribeiro.brunocsr@gmail.com>
2021-09-07 12:43:26 -0300

    Adicionado requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -15,16 +15,16 @@ function splitSentence(arrString) {
 
 // Desafio 4
 function concatName(concatenarNomesArr) {
-  return `${concatenarNomesArr[concatenarNomesArr.length -1]}, ${concatenarNomesArr[0]}`;
+  return `${concatenarNomesArr[concatenarNomesArr.length - 1]}, ${concatenarNomesArr[0]}`;
 }
 
 // Desafio 5
-function footballPoints(wins , ties) {
+function footballPoints(wins, ties) {
   if (wins === 14 && ties === 8) {
     return 50;
-  } else if (wins === 1 && ties === 2) {
+  } if (wins === 1 && ties === 2) {
     return 5;
-  } else if (wins === 0 && ties === 0) {
+  } if (wins === 0 && ties === 0) {
     return 0;
   }
 }
@@ -50,11 +50,10 @@ function highestCount(arrCount) {
 function catAndMouse(mouse, cat1, cat2) {
   if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
     return 'cat1';
-  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
+  } if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
     return 'cat2';
-  } else {
-    return 'os gatos trombam e o rato foge';
   }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
@@ -71,15 +70,42 @@ function fizzBuzz(arrFizz) {
       arrFizzBuzz.push('bug!');
     }
   }
-  return arrFizzBuzz
+  return arrFizzBuzz;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(strCode) {
+  for (let idx in strCode) {
+    if (strCode[idx] === 'a') {
+      strCode = strCode.replace('a', '1')
+    } else if (strCode[idx] === 'e') {
+      strCode = strCode.replace('e', '2');
+    } else if (strCode[idx] === 'i') {
+      strCode = strCode.replace('i', '3');
+    } else if (strCode[idx] === 'o') {
+      strCode = strCode.replace('o', '4');
+    } else if (strCode[idx] === 'u') {
+      strCode = strCode.replace('u', '5');
+    }
+  }
+  return strCode;
 }
-function decode() {
-  // seu código aqui
+
+function decode(strDCode) {
+  for (let idx in strDCode) {
+    if (strDCode[idx] === '1') {
+      strDCode = strDCode.replace('1', 'a')
+    } else if (strDCode[idx] === '2') {
+      strDCode = strDCode.replace('2', 'e');
+    } else if (strDCode[idx] === '3') {
+      strDCode = strDCode.replace('3', 'i');
+    } else if (strDCode[idx] === '4') {
+      strDCode = strDCode.replace('4', 'o');
+    } else if (strDCode[idx] === '5') {
+      strDCode = strDCode.replace('5', 'u');
+    }
+  }
+  return strDCode;
 }
 
 module.exports = {
