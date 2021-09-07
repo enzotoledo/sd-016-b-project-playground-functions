8bf82006076aac687c2e0a11ea70313888cb0e89
natashiaromanzini <natashiaromanzini1@gmail.com>
2021-09-07 08:52:33 -0300

    mudando

--- src/challenges.js
+++ src/challenges.js
@@ -18,7 +18,7 @@ function splitSentence() {
 
 // Desafio 4
 function concatName(ultimoItem, primeiroItem) {
-  let ultimoItem = concatName[concatName.length-1];
+  
   let primeiroItem = concatName[0];
   let resultado = concatName.concat(ultimoItem, primeiroItem);
   return resultado;  
@@ -32,26 +32,23 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount() {
   let nMaior = Math.max (highestCount);
-  for ( let i=0; i < nMaior.length; nMaior++;)
+  for ( let i=0; i < nMaior.length; nMaior++)
   return nMaior++;
 }
 
 // Desafio 7
 function catAndMouse(cat1, cat2) {
   if (cat2<cat1){
-  console.log "cat2"};
- if else( cat1<cat2);
- {console.log "cat1"};
+  console.log ("cat2")}
+ else if ( cat1<cat2)
+ {console.log ("cat1")}
  else{
-  console.log "os gatos trombam e o rato foge"};
-
-  
+  console.log ("os gatos trombam e o rato foge")}  
 }
 
 // Desafio 8
 function fizzBuzz(n) {
-  for (n <= 45) {
-    if(n % 3 == 0 && n % 5 == 0){
+      if(n % 3 == 0 && n % 5 == 0){
         console.log(n +" FizzBuzz");
     }else if(n % 5 == 0){
         console.log(n +" Buzz");
