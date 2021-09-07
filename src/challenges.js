7aeb44ad73e0fc3ec3ebcc6a8eca5b2a38ac7eaa
Seu nome <s.drksilva@gmail.com>
2021-09-07 04:09:27 -0300

    ˜Finalized challenge 9

--- src/challenges.js
+++ src/challenges.js
@@ -79,11 +79,43 @@ function fizzBuzz(value) {
 }
 
 // Desafio 9
-function encode() {
+function encode(value) {
   // seu código aqui
+  let array = value.split("");
+  
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "a") {
+      array[i] = 1;
+    } else if (array[i] === "e") {
+      array[i] = 2;
+    } else if (array[i] === "i") {
+      array[i] = 3;
+    } else if (array[i] === "o") {
+      array[i] = 4;
+    } else if (array[i] === "u") {
+      array[i] = 5;
+    }
+  }
+
+  return array.join("");
 }
-function decode() {
+function decode(value) {
   // seu código aqui
+  let array = value.split("");
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === '1') {
+      array[i] = "a";
+    } else if (array[i] === '2') {
+      array[i] = "e";
+    } else if (array[i] === '3') {
+      array[i] = "i";
+    } else if (array[i] === '4') {
+      array[i] = "o";
+    } else if (array[i] === '5') {
+      array[i] = "u";
+    }
+  }
+  return array.join("");
 }
 
 module.exports = {
