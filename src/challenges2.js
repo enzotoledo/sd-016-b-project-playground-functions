b4da8bdf989f22c73c9a23803d0cf9f0224e3752
MychaelJales <mychael_duarte@hotmail.com>
2021-09-06 23:04:19 -0300

    corrigindo erros do lint em challenges2

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,42 +17,70 @@ function techList(arrayTechs, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber(arrayNumbers) {
-  let phoneNumber = '';
-  
+function testRepeatIncrement(arrayNumbers, number) {
   let repeatTest = 0;
-  
+
+  for (let index2 = 0; index2 < arrayNumbers.length; index2 += 1) {
+    if (arrayNumbers[index2] === number) {
+      repeatTest += 1;
+    }
+  }
+
+  return repeatTest;
+}
+
+function repeatTestFunction(arrayNumbers) {
+  let repeatTest = 0;
+
   let repeatTest2 = false;
-  
-  let numbersIr = false;
 
   for (let index = 0; index < arrayNumbers.length; index += 1) {
-    for (let index2 = 0; index2 < arrayNumbers.length; index2 += 1) {
-      if (arrayNumbers[index2] === arrayNumbers[index]) {
-        repeatTest += 1;
-      }
-    }
+    repeatTest = testRepeatIncrement(arrayNumbers, arrayNumbers[index]);
 
     if (repeatTest >= 3) {
       repeatTest2 = true;
     }
 
-    if ((arrayNumbers[index] < 0) || (arrayNumbers[index] > 9)) {
-      numbersIr = true;
-    }
-
     repeatTest = 0;
   }
 
+  return repeatTest2;
+}
+
+function numbersIrFunction(arrayNumbers) {
+  let numbersIr = false;
+
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if ((arrayNumbers[index] < 0) || (arrayNumbers[index] > 9)) {
+      numbersIr = true;
+    }
+  }
+
+  return numbersIr;
+}
+
+function organizenumber(Numbers) {
+  let parleft = '(';
+  let parRight = ')';
+  let space = ' ';
+  let trace = '-';
+  let DDD = (parleft + Numbers[0] + Numbers[1] + parRight);
+  let fiveNumbers = space + Numbers[2] + Numbers[3] + Numbers[4] + Numbers[5] + Numbers[6];
+  let fourNumbers = trace + Numbers[7] + Numbers[8] + Numbers[9] + Numbers[10];
+
+  return DDD + fiveNumbers + fourNumbers;
+}
+
+function generatePhoneNumber(arrayNumbers) {
+  let phoneNumber = '';
+  let repeatTest = repeatTestFunction(arrayNumbers);
+  let numbersIr = numbersIrFunction(arrayNumbers);
   if (arrayNumbers.length !== 11) {
     phoneNumber = 'Array com tamanho incorreto.';
-  } else if ((repeatTest2 === true) || (numbersIr === true)) {
+  } else if ((repeatTest === true) || (numbersIr === true)) {
     phoneNumber = 'não é possível gerar um número de telefone com esses valores';
   } else {
-    let DDD = ('(' + arrayNumbers[0] + arrayNumbers[1] + ')');
-    let fiveNumbers = ' ' + arrayNumbers[2] + arrayNumbers[3] + arrayNumbers[4] + arrayNumbers[5] + arrayNumbers[6];
-    let fourNumbers = '-' + arrayNumbers[7] + arrayNumbers[8] + arrayNumbers[9] + arrayNumbers[10];
-    phoneNumber = DDD + fiveNumbers + fourNumbers;
+    phoneNumber = organizenumber(arrayNumbers);
   }
 
   return phoneNumber;
@@ -82,19 +110,19 @@ function triangleCheck(lineA, lineB, LineC) {
 
 // Desafio 13
 function hydrate(numberDrinks) {
-  //let regexp = /\d+/g;
-
   let matchesArray = numberDrinks.match(/\d+/g).map(Number);
 
   let cupsWatter = 0;
 
   let numberCupsWatter = 0;
 
+  let cupsWatterString = ' copos de água';
+
   for (let index = 0; index < matchesArray.length; index += 1) {
     cupsWatter += matchesArray[index];
   }
   if (cupsWatter > 1) {
-    numberCupsWatter = cupsWatter + ' copos de água';
+    numberCupsWatter = cupsWatter + cupsWatterString;
   } else {
     numberCupsWatter = '1 copo de água';
   }
