45fe98603a76719b786204c8950433fa5ce50ae5
RonnyMV <rjmv.dev@gmail.com>
2021-09-08 12:10:00 -0300

    10 , 13 Challenge2

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,19 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, name) {
+  let arraySort = array.sort();
+  let newArray = [];
+  let objectList = {};
+
+  for (let index = 0; index < array.length; index += 1) {
+    objectList.tech = arraySort[index];
+    objectList.name = name;
+    newArray.push(objectList);
+    objectList = {};
+  }
+  if (newArray.length === 0) {
+    newArray = 'Vazio!';
+  }
+  return newArray;
 }
 
 // Desafio 11
@@ -14,8 +27,18 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+// Consultei este tutorial para retirar numero de uma string : https://www.youtube.com/watch?v=pfkkdzeyx6U&ab_channel=AllThingsJavaScript%2CLLC
+// consultei este tutorial para aprender sobre o .map https://www.youtube.com/watch?v=G3BS3sh3D8Q&ab_channel=ProgrammingwithMosh
+function hydrate(string) {
+  let water = 0;
+  let stringNumbers = string.match(/\d+/g).map(Number);
+  for (let number of stringNumbers) {
+    water += number;
+  }
+  if (water === 1) {
+    return `${water} copo de água`;
+  }
+  return `${water} copos de água`;
 }
 
 module.exports = {
