66cd113865b06e508aa4aa4a5cc243ce31b65f24
Seu nome <s.drksilva@gmail.com>
2021-09-07 22:26:09 -0300

    Finalized challange 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,49 @@
 // Desafio 10
-function techList() {
+function techList(array, name) {
   // seu código aqui
+  let listObj = [];
+  let list = array.sort();
+  if (!(array.length === 0)) {
+    for (let i in list) {
+      listObj.push({
+        tech: list[i],
+        name,
+      });
+    }
+    return listObj;
+  } else {
+    return "Vazio!";
+  }
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(value) {
   // seu código aqui
+  unk = "não é possível gerar um número de telefone com esses valores";
+  if (value.length === 11) {
+    for (let k of value) {
+      let r = 0;
+      if (k < 0 || k > 9) {
+        return unk
+      }
+      for (let j of value ){
+        if (k === j)
+          r++;
+      }
+      if (r >= 3) {
+        return unk;
+      }
+
+    }
+
+    value.splice(7, 0, '-'); 
+    value.splice(0, 0, '(');
+    value.splice(3, 0, ')');
+    value.splice(4, 0, ' ');
+    return value.join("");
+  } else {
+    return "Array com tamanho incorreto.";
+  }
 }
 
 // Desafio 12
